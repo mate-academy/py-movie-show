e33a131943604d4ce24e3948a6050dc7ee3c770d
TJgkOsVP <sasha06052001@gmail.com>
2022-01-24 17:22:29 +0200

    'Solution'

---
+++ app/cinema/bar.py
@@ -0,0 +1,4 @@
+class CinemaBar:
+    @staticmethod
+    def sell_product(costumer, product):
+        print(f"Cinema bar sold {product} to {costumer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    for customer in customers:
+        CinemaBar.sell_product(Customer(customer["name"],
+                                        customer["food"]),
+                               Customer(customer["name"],
+                                        customer["food"]).food,)
+    return CinemaHall(hall_number).movie_session(movie, customers, cleaner)
