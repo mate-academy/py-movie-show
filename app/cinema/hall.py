61bb5d4bd72beeae7fd6f4b4a6f6c6a7faec657c
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-25 06:25:21 +0200

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,11 +1,8 @@
-from app.people.cinema_staff import Cleaner
-
-
 class CinemaHall:
     def __init__(self, number):
         self.number = number
 
-    def movie_session(self, movie_name, customers, cleaning_staff=Cleaner):
+    def movie_session(self, movie_name, customers, cleaning_staff):
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
