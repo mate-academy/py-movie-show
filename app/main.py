3cb9c482d93e1144f3aed418d698ecc8154698e7
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-05-19 10:59:14 +0100

    Solution corrected

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ from app.people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     list_of_customers = []
+    cinema_bar = CinemaBar()
     for dic in customers:
         person = Customer(name=dic["name"], food=dic["food"])
         list_of_customers.append(person)
-        cinema_bar = CinemaBar()
         cinema_bar.sell_product(customer=person, product=person.food)
     cinema_hall = CinemaHall(number=hall_number)
     cinema_cleaner = Cleaner(name=cleaner)
