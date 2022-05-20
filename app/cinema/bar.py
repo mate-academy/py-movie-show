ba92959db6f145306defa57d5a115723b5797ba5
ILIAS <nossudak@gmail.com>
2022-05-20 20:51:15 +0300

    Solution

---
+++ app/cinema/bar.py
@@ -0,0 +1,7 @@
+class CinemaBar:
+    def __init(self):
+        pass
+
+    @staticmethod
+    def sell_product(customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
