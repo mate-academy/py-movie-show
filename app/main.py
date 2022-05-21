08b34d1adbd3e017272e7dd704e085c008b32c2a
Олег Белый <belochard@gmail.com>
2022-05-21 23:03:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customers_object = []
+    for customer in customers:
+        customer_obj = Customer(customer["name"], customer["food"])
+        customers_object.append(customer_obj)
+
+    for cust_obj in customers_object:
+        CinemaBar.sell_product(cust_obj, cust_obj.food)
+
+    occupiedCleaner = Cleaner(cleaner)
+    ch = CinemaHall(hall_number)
+
+    # ask mentor why "self" here demands object "ch"
+    CinemaHall.movie_session(ch, movie, customers_object, occupiedCleaner)
