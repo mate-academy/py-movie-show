ef120927e5082bab13931f86825395ea88b69c2e
Elena <lenasharabura@gmail.com>
2022-05-24 13:22:56 +0200

    add annotation, change quotes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -2,7 +2,7 @@ from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
-    def __init__(self, number):
+    def __init__(self, number: int):
         self.number = number
 
     def movie_session(
@@ -14,5 +14,5 @@ class CinemaHall:
         print(f"\"{movie_name}\" started in hall number {self.number}.")
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f"\"{movie_name}\" ended.")
+        print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
