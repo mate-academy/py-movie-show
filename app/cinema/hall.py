0f760de086d7c50635f97d5dac3a618f9cba0229
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-05-24 16:25:16 +0300

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -11,7 +11,7 @@ class CinemaHall:
             customers: list,
             cleaning_staff: Cleaner
     ):
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f"\"{movie_name}\" ended.")
