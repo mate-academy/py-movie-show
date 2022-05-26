5d11ac687adf56c269888d57de7aaa85d553941e
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-26 19:08:58 +0300

    'Fixes'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,7 +6,6 @@ class CinemaHall:
         print(f"\"{movie_name}\" started in hall number {self.number}.")
 
         for customer in customers:
-            b = customer
-            b.watch_movie(movie_name)
+            customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
