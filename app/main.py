f07221150c642ec861a987b79693afbbafb4f1b0
Alexandr <netiazhevskyi@gmail.com>
2022-06-16 16:02:25 +0300

    one test not passing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customers_list = []
+    for i in customers:
+        customer = Customer(i.get("name"), i.get("food"))
+        customers_list.append(customer)
+        CinemaBar.sell_product(customer.food, customer)
+    cinema_hall = CinemaHall(hall_number)
+    cleaning_person = Cleaner(cleaner)
+    CinemaHall.movie_session(cinema_hall, movie,
+                             customers_list, cleaning_person)
