cafa1062e276c51da58b807e0d8d1869746f4b15
Alexandr <netiazhevskyi@gmail.com>
2022-06-16 23:36:49 +0300

    tests passed, move cinemabar our of the loop

--- app/main.py
+++ app/main.py
@@ -6,10 +6,11 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     customers_list = []
+    cinema_bar = CinemaBar()
     for i in customers:
         customer = Customer(i.get("name"), i.get("food"))
         customers_list.append(customer)
-        CinemaBar.sell_product(customer.food, customer)
+        cinema_bar.sell_product(customer.food, customer)
     cinema_hall = CinemaHall(hall_number)
     cleaning_person = Cleaner(cleaner)
     cinema_hall.movie_session(movie, customers_list, cleaning_person)
