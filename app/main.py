1136907141a8f0bf96debf6acc6ded438974dc47
Tetiana Omelchenko <t.omel4enko@gmail.com>
2022-06-30 12:49:29 +0800

    Removed extra CinemaBars, returned type annotation

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     customer_instances = []
     for customer in customers:
         customer_instance = Customer(customer["name"], customer["food"])
         customer_instances.append(customer_instance)
-        CinemaBar.sell_product(customer_instance, customer["food"])
+    bar = CinemaBar()
+    for instance in customer_instances:
+        bar.sell_product(instance, instance.food)
     hall = CinemaHall(hall_number)
     staff = Cleaner(cleaner)
     CinemaHall.movie_session(hall, movie, customer_instances, staff)
