126c8c3b63f0ea1963132a7636a7b743805c79c5
Misha Hrynko <misha@mate.academy>
2022-06-30 13:19:40 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers, hall_number, cleaner, movie):
+    ls_of_cls_obj = [Customer(cus["name"], cus["food"]) for cus in customers]
+    cleaner = Cleaner(cleaner)
+    hall_number = CinemaHall(hall_number)
+    for i in ls_of_cls_obj:
+        CinemaBar.sell_product(i, i.food)
+    CinemaHall.movie_session(hall_number, movie, ls_of_cls_obj, cleaner)
