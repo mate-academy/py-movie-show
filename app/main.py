35e5333183c61ab9976c53232401a33d8f99ec6a
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-06-30 20:56:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.cinema_staff import Cleaner
-from app.people.customer import Customer
+
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     number = 0
