9a42b15314d2782b1f08299465bb054b6eb91844
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-06-30 21:48:02 +0300

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,7 +8,6 @@ class CinemaHall:
 
     def movie_session(self, movie_name: str, customers: list, cleaning_staff: str):
         print(f'{movie_name} started in hall number {self.number}.')
-
         number = 0
         for _ in customers:
             customer_name = customers[number]['name']
@@ -16,8 +15,6 @@ class CinemaHall:
             customer = Customer(customer_name, customer_food)
             customer.watch_movie(movie_name)
             number += 1
-
         print(f'{movie_name} ended.')
-
         clean = Cleaner(cleaning_staff)
         clean.clean_hall(self.number)
