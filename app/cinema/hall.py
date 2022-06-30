0e79914cc11297166c157bf32214cb2ce9f56dc1
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-06-30 20:50:22 +0300

    MY_Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,22 @@
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+class CinemaHall:
+    def __init__(self, number: int):
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers: list, cleaning_staff: str):
+        print(f'{movie_name} started in hall number {self.number}.')
+
+        number = 0
+        for _ in customers:
+            customer_name = customers[number]['name']
+            customer_food = customers[number]['food']
+            customer = Customer(customer_name, customer_food)
+            customer.watch_movie(movie_name)
+            number += 1
+
+        print(f'{movie_name} ended.')
+
+        clean = Cleaner(cleaning_staff)
+        clean.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-# write your imports here
-
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    number = 0
+    for _ in customers:
+        customer_name = customers[number]['name']
+        customer_food = customers[number]['food']
+        CinemaBar.sell_product(customer_food, customer_name)
+        number += 1
+    session = CinemaHall(hall_number)
+    session.movie_session(movie_name=movie, customers=customers, cleaning_staff=cleaner)
+
+
+
+customers = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
+hall_number = 5
+cleaner_name = "Anna"
+movie = "Madagascar"
+
+cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
+
+
+
+
+# Cinema bar sold Coca-cola to Bob.
+# Cinema bar sold popcorn to Alex.
+# "Madagascar" started in hall number 5.
+# Bob is watching "Madagascar".
+# Alex is watching "Madagascar".
+# "Madagascar" ended.
+# Cleaner Anna is cleaning hall number 5.
