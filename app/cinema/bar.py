87222573e26f483d6dcd9840a275a88e2ff5e5e4
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-01 07:16:12 +0300

    Update bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,4 @@
 class CinemaBar:
     @staticmethod
     def sell_product(product: str, customer: str):
-            print(f"Cinema bar sold {product} to {customer}")
+        print(f"Cinema bar sold {product} to {customer}")
