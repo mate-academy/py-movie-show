4dd2d72231009c6234fefbf5cca15ae129dcb823
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-01 07:14:35 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,14 +15,3 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
         number += 1
     session = CinemaHall(hall_number)
     session.movie_session(movie_name=movie, customers=customers, cleaning_staff=cleaner)
-
-
-customers = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-
-cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
