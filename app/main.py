cfe2e9b4e96c86d2ce824e5e5c4f3ea10f542cf4
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-26 16:48:58 +0300

    New

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    cinema_bar_obj = CinemaBar
+    cinema_bar_obj = CinemaBar()
     cleaner_obj = Cleaner(cleaner)
     list_of_customers = []
 
