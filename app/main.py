ce6a75ccbba6e6de632f65ff13fa0bfd19f3032f
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-26 15:22:23 +0300

    I dont understand what I need to change

--- app/main.py
+++ app/main.py
@@ -7,9 +7,13 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     cleaner_obj = Cleaner(cleaner)
     list_of_customers = []
-    for customer in customers:
-        cs = Customer(customer["name"], customer["food"])
+
+    for customer_not_obj in customers:
+        cs = Customer(customer_not_obj["name"], customer_not_obj["food"])
         list_of_customers.append(cs)
+
+    for cs in list_of_customers:
         CinemaBar.sell_product(cs, cs.food)
+
     cinema_hall = CinemaHall(hall_number)
     cinema_hall.movie_session(movie, list_of_customers, cleaner_obj)
