640ddef8637df8e776c3d7446887be6fa1ec5ae1
AnDy <ansmbox@gmail.com>
2022-07-28 11:20:49 +0300

    use '' instead of "" in print()

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,11 +7,11 @@ class CinemaHall:
 
     def movie_session(self, movie_name: str, customers: list,
                       cleaning_staff: cinema_staff.Cleaner):
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f"\"{movie_name}\" ended.")
+        print(f'"{movie_name}" ended.')
 
         cleaning_staff.clean_hall(self.number)
