4724f0c58062e922d112439e358deb45327392e3
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-08-03 18:26:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    num_halls = CinemaHall(hall_number)
+    cleaner_name = Cleaner(cleaner)
+    cinema_guest = []
+    for customer in customers:
+        customer = Customer(name=customer["name"], food=customer["food"])
+        cinema_guest.append(customer)
+        CinemaBar.sell_product(customer.food, customer)
+    CinemaHall.movie_session(num_halls, movie, cinema_guest, cleaner_name)
---
+++ app/people/customer.py
@@ -0,0 +1,12 @@
+class Customer:
+    def __init__(self, name: str, food: str):
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str):
+        print(f"{self.name} is watching \"{movie}\".")
+
+
+if __name__ == '__main__':
+    bob = Customer(name="Bob", food="popcorn")
+    print(bob.watch_movie(movie="Madagascar"))
