eee51bff7bc43079d4c42d25f018ea8126d3c789
Victor <ruravictor@gmail.com>
2022-08-15 16:47:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-# write your imports here
+from app.cinema.bar import
+from app.cinema.hall import class CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+
