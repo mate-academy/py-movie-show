a7600167b95006608bc628f5cd0016d0d5f77d2d
Victor <ruravictor@gmail.com>
2022-08-16 12:36:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ from app.people.customer import Customer
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     hall_number = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
-    custom_list = []
+    customer_list = []
     for customer in customers:
         customer = Customer(customer["name"], customer["food"])
         CinemaBar.sell_product(customer.food, customer)
-        custom_list.append(customer)
-    CinemaHall.movie_session(hall_number, movie, custom_list, cleaner)
+        customer_list.append(customer)
+    CinemaHall.movie_session(hall_number, movie, customer_list, cleaner)
