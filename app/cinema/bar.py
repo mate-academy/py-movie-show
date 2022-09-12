8393cfe6934eb3b4140b5028edf541dfaebfa954
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-09-12 13:21:50 +0300

    solution v02 (using "" instead of '')

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,4 +4,4 @@ class CinemaBar:
 
     @staticmethod
     def sell_product(customer, product) -> None:
-        print(f'Cinema bar sold {product} to {customer.name}.')
+        print(f"Cinema bar sold {product} to {customer.name}.")
