3aed053f3f306a527287c1fc9635859b2a90e5b3
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-09-15 17:07:47 +0300

    fixes after review

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     customers = [
-        Customer(customer["name"], customer["food"]) for customer in customers
+        Customer(customer['name'], customer['food']) for customer in customers
     ]
 
     for customer in customers:
