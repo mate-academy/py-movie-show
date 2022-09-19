52060123224b52bec146698139bc022126ae58e4
Stas Okhrym <s.okhrym@gmail.com>
2022-09-19 14:36:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     customers = [
-        Customer(customer['name'], customer['food']) for customer in customers
+        Customer(customer["name"], customer["food"]) for customer in customers
     ]
     cleaning_staff = Cleaner(cleaner)
     cinema_hall = CinemaHall(hall_number)
