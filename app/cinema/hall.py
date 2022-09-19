5a2c193ca4d9762b49b25e8531b9ef5644ff3fd8
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-19 12:41:01 +0300

    Update hall.py
    add anotation

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,8 +1,8 @@
 class CinemaHall:
-    def __init__(self, number):
+    def __init__(self, number: int):
         self.number = number
 
-    def movie_session(self, movie_name, customers, cleaning_staff):
+    def movie_session(self, movie_name: str, customers: list, cleaning_staff: Cleaner) -> str:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for person in customers:
             person.watch_movie(movie_name)
