859fa50def645271d1e0b79807e065614df45e3b
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-19 12:42:40 +0300

    Update main.py
    add anotation

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     for index in range(len(customers)):
         customers[index] = Customer(customers[index]["name"]
                                     , customers[index]["food"])
