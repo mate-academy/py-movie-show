4d3fc7cbfa9aec382ef86c037f394ecbf1120a63
igor kosianenko <funkcionalkos@gmail.com>
2022-09-20 20:33:23 +0300

    comprehension changed to simle 'for'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,7 +6,8 @@ class CinemaHall():
 
         print(f'"{movie_name}" started in hall number {self.number}.')
 
-        [customer.watch_movie(movie_name) for customer in customers]
+        for customer in customers:
+            customer.watch_movie(movie_name)
 
         print(f'"{movie_name}" ended.')
 
--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
         for person in customers
     ]
 
-    [CinemaBar.sell_product(customer, customer.food)
-     for customer in obj_customers]
+    for customer in obj_customers:
+        CinemaBar.sell_product(customer, customer.food)
+    # [CinemaBar.sell_product(customer, customer.food)
+    #  for customer in obj_customers]
 
     CinemaHall(hall_number).movie_session(
         movie, obj_customers , Cleaner(cleaner)
