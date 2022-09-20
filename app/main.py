4f5896210b172116326952e23471bdc7cd5a4aac
shurushku <nastia.kuchina.741456963@gmail.com>
2022-09-20 16:31:01 +0300

    add type annotations

--- app/main.py
+++ app/main.py
@@ -4,14 +4,13 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     list_of_customers = [
         Customer(customer['name'], customer['food'])
         for customer in customers
     ]
     cinema = CinemaHall(hall_number)
 
-    for customer in list_of_customers:
-        CinemaBar.sell_product(customer.food, customer)
+    print([CinemaBar.sell_product(customer.food, customer) for customer in list_of_customers])
 
     cinema.movie_session(movie, list_of_customers, Cleaner(cleaner))
