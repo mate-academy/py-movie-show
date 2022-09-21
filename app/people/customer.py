a37a760ba586b7095b79593d9ebafa9ba02553dd
Sergii Kantor <sergiy.kantor@gmail.com>
2022-09-21 14:04:48 +0300

    Skantor re-solving

--- app/people/customer.py
+++ app/people/customer.py
@@ -5,7 +5,3 @@ class Customer:
 
     def watch_movie(self, movie):
         print(f"{self.name} is watching \"{movie}\".")
-
-
-# bob = Customer(name="Bob", food="popcorn")
-# bob.watch_movie(movie="Madagascar")
