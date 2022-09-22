18a7b4c2f91b3bb0a324163f55f02e695d3a3193
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-09-22 11:58:11 +0300

    formatting fixed

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,10 +5,11 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(
-            self, movie_name: str,
-            customers: list,
-            cleaning_staff: Cleaner) -> None:
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list,
+                      cleaning_staff: Cleaner
+                      ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for person in customers:
