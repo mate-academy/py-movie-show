60c506aea4ff3aea5630fa830b347b72416b2857
Hryhorii Stakhiv <gsstahiv@gmail.com>
2022-09-22 14:12:24 +0300

    Fixed a grammatical error

--- app/people/customer.py
+++ app/people/customer.py
@@ -3,5 +3,5 @@ class Customer:
         self.name = name
         self.food = food
 
-    def watch_movie(self, movi: str) -> None:
-        print(f"{self.name} is watching \"{movi}\".")
+    def watch_movie(self, movie: str) -> None:
+        print(f"{self.name} is watching \"{movie}\".")
