8f66334046d1953e9e360ea0542bbbc24c3f46ad
Nikyxa <kolisova.veronika81@gmail.com>
2022-09-22 21:13:11 +0300

    'Solution'

---
+++ app/people/customer.py
@@ -0,0 +1,10 @@
+class Customer:
+    def __init__(self,
+                 name: str,
+                 food: str
+                 ) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str):
+        print(f'{self.name} is watching "{movie}".')
