71af98ff1d0ee3b33b4f900eb7505ee8a274342d
mltdll <ilya.vyplavin@gmail.com>
2022-09-22 17:30:27 +0300

    Replace single quotes with double quotes

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     bar = CinemaBar()
     hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
-    customers = [Customer(customer['name'], customer['food'])
+    customers = [Customer(customer["name"], customer["food"])
                  for customer in customers]
 
     for customer in customers:
