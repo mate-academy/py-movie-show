ca5442dff9a9509c6035bd2df7614552ec779339
Kirill Drago <dragoswim7@gmail.com>
2022-09-23 11:49:12 +0300

    Fixed type annotation in CineHall constructor

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -2,7 +2,7 @@ from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
-    def __init__(self, number: str) -> None:
+    def __init__(self, number: int) -> None:
         self.number = number
 
     def movie_session(self, movie_name: str,
