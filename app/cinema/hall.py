fdf55c62b8a80f8e3501355e1830121b7f1e23a4
Sergey <servizah@gmail.com>
2022-09-23 17:00:54 +0300

    annotations added

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -2,11 +2,11 @@ from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
-    def __init__(self, number) -> None:
+    def __init__(self, number: int) -> None:
         self.number = number
 
     def movie_session(
-            self, movie_name, customers: list, current_cleaner: Cleaner
+            self, movie_name: str, customers: list, current_cleaner: Cleaner
     ) -> None:
         print(f"\"{movie_name}\" started in hall number {self.number}.")
         for person in customers:
