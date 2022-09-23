fd7a7c45c4cb04e3210ccc50d8160aad3e701bc7
Yura <pazyn404@gamil.com>
2022-09-23 10:37:01 +0300

    Fixed import annotaions and brake new line

--- app/main.py
+++ app/main.py
@@ -7,8 +7,7 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
-                 movie: str
-                 ) -> None:
+                 movie: str) -> None:
 
     hall = CinemaHall(hall_number)
     bar = CinemaBar()
