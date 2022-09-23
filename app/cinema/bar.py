7871b0a4f1abee3ab676e6620867350b4798f139
Dima Ursatii <109001524+Menselface@users.noreply.github.com>
2022-09-23 20:28:02 +0300

    Fix

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,7 +3,7 @@ from app.people.customer import Customer
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: Customer, product: str):
+    def sell_product(customer: Customer, product: str) -> None:
         product = product
         customer = customer
         print(f"Cinema bar sold {product} to {customer.name}.")
