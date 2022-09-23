6faee3a00050209c1593ee342b8de0c8c564ba0b
VitaliiPiaskivskyi <Vitaliipiaskivskyi@gmail.com>
2022-09-23 19:10:16 +0300

    Corrected_23_09_v_2

--- app/main.py
+++ app/main.py
@@ -7,12 +7,14 @@ from app.people.customer import Customer
 def cinema_visit(
         customers: list, hall_number: int, cleaner: str, movie: str
 ) -> None:
-    cb_class = CinemaBar()
-    ch_class = CinemaHall(hall_number)
-    cl_class = Cleaner(cleaner)
-    list_of_cast = []
-    for cast in customers:
-        list_of_cast.append(Customer(cast.get("name"), cast.get("food")))
-    for one_cast in list_of_cast:
-        cb_class.sell_product(one_cast.food, one_cast)
-    ch_class.movie_session(movie, list_of_cast, cl_class)
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(hall_number)
+    cinema_cleaner = Cleaner(cleaner)
+    list_of_customers = []
+    for one_cust in customers:
+        list_of_customers.append(
+            Customer(one_cust.get("name"), one_cust.get("food"))
+        )
+    for one_cast in list_of_customers:
+        cinema_bar.sell_product(one_cast.food, one_cast)
+    cinema_hall.movie_session(movie, list_of_customers, cinema_cleaner)
