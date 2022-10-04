9bd131979eebc2f4a5a4060c7e1609fad5307d30
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 21:30:24 +0300

    customer_list

--- app/main.py
+++ app/main.py
@@ -7,4 +7,10 @@ from app.people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     # write you code here
-    pass
+    customers_list = []
+    for customer_dict_elements in customers:
+        customer = Customer(customer_dict_elements["name"],
+                             customer_dict_elements["food"])
+        customers_list.append(customer)
+    for customer in customers_list:
+        CinemaBar.sell_product(customer.food, customer)
