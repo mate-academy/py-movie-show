1b58a04aba04ef3a016b5ef28ea96da4daff6e5d
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 20:31:32 +0300

    import main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 # write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
