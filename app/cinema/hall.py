29741c4ab445af3d6a82b3ee2390e5f04b21bd05
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 20:28:14 +0300

    3

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,6 +10,6 @@ class CinemaHall:
                       cleaning_staff):
         print(f"{movie_name} start at hall number {self.number}")
         for customer in customers:
-            customer.watch_movie{movie_name}
+            customer.watch_movie(movie_name)
         print(f"{movie_name} ended.")
         cleaning_staff.clean_hall(self.number)
