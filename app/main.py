391ae6f2e8682714417f4f9cdec8105000f55274
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 21:46:12 +0300

    final version

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# write your imports here
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
@@ -6,7 +5,6 @@ from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
     customers_list = []
     for customer_dict_elements in customers:
         customer = Customer(customer_dict_elements["name"],
@@ -14,3 +12,7 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
         customers_list.append(customer)
     for customer in customers_list:
         CinemaBar.sell_product(customer.food, customer)
+
+    hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner)
+    hall.movie_session(movie, customers_list, cleaner)
