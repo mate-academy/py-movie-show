55534ae453195a8235aa6a2bb062c4c679aabaa7
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 19:18:29 +0300

    customer pakage

---
+++ app/people/customer.py
@@ -0,0 +1,9 @@
+class Customer:
+
+    def __init__(self, name, food):
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie):
+        print(f"{self.name}" watch {movie}")
+    
