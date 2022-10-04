7d5f303152d0b02ca6c0dcdf71a3de17ed8d9637
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-04 20:21:31 +0300

    import changes

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,5 +1,7 @@
-class CinemaBar:
+from app.people.customer import Customer
 
+class CinemaBar:
+    
     @staticmethod
-    def sell_product(product, customer):
+    def sell_product(product: str, customer: Customer):
         print(f"CinemaBar sold {product} to {customer.name}")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,6 @@
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
 class CinemaHall:
 
     def __init__(self, number):
@@ -6,7 +9,8 @@ class CinemaHall:
     def movie_session(self, movie_name, 
                       customers: list, 
                       cleaning_staff):
-        print("Movie start")
-        watch_movie()
-        print("Movie end")
-        clean_hall()
+        print(f"{movie_name} start at hall number {self.number}")
+        for customer in customers:
+            customer.watch_movie{movie_name}
+        print(f"{movie_name} ended.")
+        cleaning_staff.clean_hall(self.number)
