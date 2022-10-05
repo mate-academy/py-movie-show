31fc3a52a7178db7d647c7d637c0409affba6493
Ivan Berezyanskyi <gamaew.res@gmail.com>
2022-10-05 16:30:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
         CinemaBar.sell_product(Customer(client['name'], client['food']),
                                client['food'])
     CinemaHall(hall_number).movie_session(movie,
-                                          [Customer(a['name'], a['food'])
-                                           for a in customers],
+                                          [Customer(q['name'], q['food'])
+                                           for q in customers],
                                           Cleaner(cleaner)
                                           )
