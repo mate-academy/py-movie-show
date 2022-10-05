ccc4da7f0653f3df0f2fd6ecad252ab67e26a85b
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-10-05 12:47:15 +0300

    solution1

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -4,5 +4,5 @@ class Cleaner:
         self.name = name
 
     def clean_hall(self, hall_number):
-        return print(f'Cleaner {self.name} is cleaning '
-                     f'hall number {hall_number}.')
+        return print(f"Cleaner {self.name} is cleaning "
+                     f"hall number {hall_number}.")
