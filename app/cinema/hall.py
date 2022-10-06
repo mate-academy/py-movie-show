8bf3d4aaac178112458a14e1b90025bb4f5f07f8
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-06 21:31:24 +0300

    flake 8 4

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,9 +1,9 @@
 class CinemaHall:
 
-    def __init__(self, number: int):
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name, customers, cleaning_staff) -> str:
+    def movie_session(self, movie_name, customers, cleaning_staff) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
