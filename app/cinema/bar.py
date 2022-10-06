d73df35d385b0f34a4e6de819cf2d80d0060f751
Denys  Tereshchuk <terryadzi83@gmail.com>
2022-10-06 17:32:23 +0400

    Solution: added annotations

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,7 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
     @staticmethod
-    def sell_product(customer, product):
+    def sell_product(customer: Customer, product: str):
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,8 +1,14 @@
+from app.people.cinema_staff import Cleaner
+
+
 class CinemaHall:
-    def __init__(self, number):
+    def __init__(self, number: int):
         self.number = number
 
-    def movie_session(self, movie_name, customers: list, cleaning_staff):
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list,
+                      cleaning_staff: Cleaner):
         print(f"\"{movie_name}\" started in hall number {self.number}.")
         for customer in customers:
             customer.watch_movie(movie_name)
