82d3b01ae64b351d9758ff3aeb18b8cf89380739
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-06 21:28:04 +0300

    flake8 1

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> str:
     actual_film = CinemaBar()
     customer_list = []
     for new_customer in customers:
