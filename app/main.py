fd5c1dc5591c57475c0575430d8440f3d899c3c6
LavrykN <nlavryk@kaaiot.io>
2022-10-07 13:54:28 +0300

    Cinema visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    visitors = []
+    for customer in customers:
+        visitor = Customer(customer["name"], customer["food"])
+        CinemaBar.sell_product(visitor.food, visitor)
+        visitors.append(visitor)
+
+    cleaner = Cleaner(cleaner)
+    CinemaHall(hall_number).movie_session(movie, visitors, cleaner)
