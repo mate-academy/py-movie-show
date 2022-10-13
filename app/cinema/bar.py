434c76faa6d64c4ddfe0f7e9254dbb57c163a8eb
Dede <specialized8393@gmail.com>
2022-10-13 14:33:27 +0300

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from app.people.customer import Customer
 
 class CinemaBar:
     @staticmethod
-    def sell_product(cust: Customer, product: str) -> None:
-        print(f"Cinema bar sold {product} to {cust.name}")
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
-from cinema import bar, hall
-from people import cinema_staff, customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     customers_list = []
+
     for cs in customers:
-        customers_list.append(customer.Customer(cs["name"], cs["food"]))
+        customers_list.append(Customer(cs["name"], cs["food"]))
+
     for cs in customers_list:
-        bar.CinemaBar.sell_product(cs, cs.food)
+        CinemaBar.sell_product(cs, cs.food)
 
-    cleaner = cinema_staff.Cleaner(cleaner)
-
-    hall.CinemaHall(hall_number).movie_session(movie, customers_list, cleaner)
+    cinema_wathing = CinemaHall(hall_number)
+    clean = Cleaner(cleaner)
+    cinema_wathing.movie_session(movie, customers_list, clean)
