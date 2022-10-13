01028e71d07067bcc4273af683738538ea6603c0
Mazur Mariya <mazurinka29@gmail.com>
2022-10-13 16:48:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -18,21 +18,3 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     CinemaHall.movie_session(cinema_instance, movie, customer_instance,
                              cleaning)
 
-
-if __name__ == '__main__':
-    customers = [
-        {"name": "Bob", "food": "Coca-cola"},
-        {"name": "Alex", "food": "popcorn"}
-    ]
-    hall_number = 5
-    cleaner = "Anna"
-    movie = "Madagascar"
-    cinema_visit(customers=customers, hall_number=5, cleaner="Anna",
-                       movie="Madagascar")
-    # Cinema bar sold Coca-cola to Bob.
-    # Cinema bar sold popcorn to Alex.
-    # "Madagascar" started in hall number 5.
-    # Bob is watching "Madagascar".
-    # Alex is watching "Madagascar".
-    # "Madagascar" ended.
-    # Cleaner Anna is cleaning hall number 5.
