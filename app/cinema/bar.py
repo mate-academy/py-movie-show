f036e7fd7e6b16203b46702694808116bbe94dd7
Mazur Mariya <mazurinka29@gmail.com>
2022-10-13 16:44:51 +0300

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,15 +1,5 @@
-# from app.people.customer import Customer
-
-
 class CinemaBar:
 
     @staticmethod
     def sell_product(customer, product):
         print(f"Cinema bar sold {product} to {customer.name}.")
-
-
-if __name__ == '__main__':
-    cb = CinemaBar()
-    customer = Customer(name="Bob", food="popcorn")
-    cb.sell_product(customer=customer, product=customer.food)
-    # Cinema bar sold popcorn to Bob.
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,7 +1,3 @@
-# from app.people.cinema_staff import Cleaner
-# from app.people.customer import Customer
-
-
 class CinemaHall:
     def __init__(self, number: int):
         self.number = number
@@ -15,21 +11,3 @@ class CinemaHall:
 
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
-
-
-if __name__ == '__main__':
-    number = 5
-    ch = CinemaHall(number)
-    movie_name = "Madagascar"
-
-    customers = [Customer(name="Bob", food="popcorn"),
-                 Customer(name="Robbert", food="cola")]
-    cleaning_staff = Cleaner(name="Anna")
-    ch.movie_session(movie_name=movie_name, customers=customers,
-                     cleaning_staff=cleaning_staff)
-
-    # "Madagascar" started in hall number 5.
-    # Bob is watching "Madagascar".
-    # Alex is watching "Madagascar".
-    # "Madagascar" ended.
-    # Cleaner Anna is cleaning hall number 5.
