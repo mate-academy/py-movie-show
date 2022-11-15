543256fbbcb52fb14ffd8dcfcf71c71588400ae3
Vladimir Titarenko <tytarenkova@gmail.com>
2022-11-15 14:42:16 +0300

    in hall.py comment was deleted, annotations were changed

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 class CinemaHall:
@@ -6,7 +8,7 @@ class CinemaHall:
         self.number = number
 
     def movie_session(self, movie_name: str,
-                      customers: list,  # list of Customer instances
+                      customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
