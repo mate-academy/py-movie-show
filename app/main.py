7aa4f611dc229b6851eee1f41f7b6218b356ccbb
Vladimir Titarenko <tytarenkova@gmail.com>
2022-11-15 13:09:43 +0300

    Unneccessary conditions, tests changes, code dublications were canceled. The correct annotation was added

--- app/main.py
+++ app/main.py
@@ -9,13 +9,9 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) \
     cinema_hall_instance = CinemaHall(hall_number)
     customer_instances = []
     for person in customers:
-        if not isinstance(person, Customer):
-            customer_instance = Customer(name=person["name"],
-                                         food=person["food"])
-        else:
-            customer_instance = person
-        CinemaBar.sell_product(customer_instance.name, customer_instance.food)
+        customer_instance = Customer(person["name"], person["food"])
         customer_instances.append(customer_instance)
+        CinemaBar.sell_product(customer_instance, customer_instance.food)
     cleaner_instance = Cleaner(cleaner)
     cinema_hall_instance.movie_session(movie_name=movie,
                                        customers=customer_instances,
