094dee72bf7dd8f2fe87e747b8d3e39e3281f5da
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-11-25 12:53:26 +0200

    Unnecessary \ and comment was removed and CinemaBar instance was created before loop

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-# write your imports here
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    custom_list = [
+        Customer(guest["name"], guest["food"])
+        for guest in customers]
+    cb = CinemaBar()
+    for person in custom_list:
+        cb.sell_product(
+            customer=person,
+            product=person.food)
+    CinemaHall(hall_number).movie_session(movie, custom_list, Cleaner(cleaner))
