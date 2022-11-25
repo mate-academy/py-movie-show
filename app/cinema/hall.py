64b6b1c895b4492c0e51bf9fa50ec0df758c6fce
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-11-25 13:01:04 +0200

    Unnecessary \ and comment was removed and CinemaBar instance was created before loop

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -13,16 +13,8 @@ class CinemaHall:
             cleaner: Cleaner
     ) -> None:
 
-<<<<<<< HEAD
         print(f'"{movie_name}" started in hall number {self.number}.')
         for person in customers:
             person.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-=======
-        print(f'\"{movie_name}\" started in hall number {self.number}.')
-        for person in customers:
-            # one_customer = Customer(**person)
-            person.watch_movie(movie_name)
-        print(f'\"{movie_name}\" ended.')
->>>>>>> 8a0cf225de1c1d9d1123ba0233a2f852bd01be29
         cleaner.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -13,14 +13,9 @@ def cinema_visit(
     custom_list = [
         Customer(guest["name"], guest["food"])
         for guest in customers]
-<<<<<<< HEAD
     cb = CinemaBar()
     for person in custom_list:
         cb.sell_product(
-=======
-    for person in custom_list:
-        CinemaBar.sell_product(
->>>>>>> 8a0cf225de1c1d9d1123ba0233a2f852bd01be29
             customer=person,
             product=person.food)
     CinemaHall(hall_number).movie_session(movie, custom_list, Cleaner(cleaner))
