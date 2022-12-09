def605d6c7d75d79149b2bfe9347878375390404
Dmytro Prokofiev <forbuffa@gmail.com>
2022-12-09 13:57:55 +0200

    Need a help

---
+++ app/cinema/bar.py
@@ -0,0 +1,6 @@
+class CinemaBar:
+    def __int__(self):
+
+    @staticmethod
+    def sell_product(customer: str, product: str):
+        print(f"Cinema bar sold {product} to {customer}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-# write your imports here
+from cinema.bar import CinemaBar
+from cinema.hall import CinemaHall
+from people.cinema_staff import Cleaner
+from people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customers = Customer(customers)
+    number = CinemaHall(hall_number)
+    movie_name = CinemaBar(movie)
+    cleaner = Cleaner(cleaner)
