cdb87b2c7de5c2b1027eb87f316c5e9ae70afbf6
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2022-12-13 19:44:48 +0200

    fix indentation

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str,
-                 movie: str) -> None:
-    customer_list = []
-    for customer in customers:
-        customer_list.append(Customer(customer["name"], customer["food"]))
+def cinema_visit(
+    customers: list, hall_number: int,
+        cleaner: str, movie: str
+) -> None:
+    customer_list = [
+        Customer(customer["name"],
+                 customer["food"]) for customer in customers]
 
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
