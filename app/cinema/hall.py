9d4f69fc41b4151ab696d4af87eef5aeaaf651ff
Anatoliy Pilipey <pilipeytolya@gmail.com>
2022-12-15 10:35:08 +0200

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list,
+            cleaner: Callable
+    ) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
+        for client in customers:
+            client.watch_movie(movie_name)
+
+        print(f'"{movie_name}" ended.')
+        cleaner.clean_hall(self.number)
