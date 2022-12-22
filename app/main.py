26f6e1436503cec7a610145e5a07b60a9b2844e3
Olena Liubynetska <olena.liuby@gmail.com>
2022-12-22 12:31:46 +0000

    Made requested changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
@@ -11,20 +10,24 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-    customers_list = []
+    customers_instances_list = []
     cleaner_instance = Cleaner(cleaner)
+    cinema_bar_instances = CinemaBar()
     hall = CinemaHall(hall_number)
 
-    for customer in customers:
+    for person in customers:
         # making Customers instances
-        customer = Customer(name=customer["name"], food=customer["food"])
-        customers_list.append(customer)
+        customer = Customer(name=person["name"],
+                            food=person["food"])
+        customers_instances_list.append(customer)
 
-        # selling food
-        CinemaBar.sell_product(customer=customer.name, product=customer.food)
+    for person in customers_instances_list:
+        # selling products
+        cinema_bar_instances.sell_product(customer=person,
+                                          product=person.food)
 
     hall.movie_session(
         movie_name=movie,
-        customers=customers_list,
+        customers=customers_instances_list,
         cleaning_staff=cleaner_instance
     )
