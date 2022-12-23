3ba769c6dfd866010904fe1f36a556618ad07fd1
Anna Kabatova <kabatova.anna@ukr.net>
2022-12-23 19:01:42 +0200

    1st try solution

---
+++ app/people/cinema_staff.py
@@ -0,0 +1,7 @@
+class Cleaner:
+
+    def __init__(self, name: str) -> callable:
+        self.name = name
+
+    def clean_hall(self, hall_number: int) -> callable:
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
