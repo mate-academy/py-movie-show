16599a2348c1a96e7c6eaa6e7eae64f000918c0e
Dibrov Maksym <dibrov.maksym@solidgate.com>
2022-12-26 20:07:48 +0100

    solition implemented

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 # write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str,
+                 movie: str) -> None:
     # write you code here
-    pass
+    customer_instances = []
+    cinema_hall = CinemaHall(hall_number)
+    cinema_bar = CinemaBar()
+    cleaner_instance = Cleaner(cleaner)
+    for i in customers:
+        customer_instances.append(Customer(i["name"], i["food"]))
+
+    for i in customer_instances:
+        cinema_bar.sell_product(i.food, i)
+    cinema_hall.movie_session(movie, customer_instances, cleaner_instance)
