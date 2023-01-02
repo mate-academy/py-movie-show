e5a75bacdc15b8bebd568a8a4a54c87d23ba08e9
Dmytro <omelchenko.dm@gmail.com>
2023-01-02 22:14:02 +0200

    Cinema_visit

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,12 +5,18 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self,
-                      movie_name: str,
-                      customers: list,
-                      cleaning_staff: Cleaner) -> None:
-        print('"{}" started in hall number {}.'.format(movie_name,
-                                                       self.number))
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list,
+            cleaning_staff: Cleaner
+    ) -> None:
+        print(
+            '"{}" started in hall number {}.'.format(
+                movie_name,
+                self.number
+            )
+        )
         for customer in customers:
             customer.watch_movie(movie_name)
         print('"{}" ended.'.format(movie_name))
--- app/main.py
+++ app/main.py
@@ -4,14 +4,20 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     customer_class_obj_list = []
     for customer in customers:
-        customer_class_obj_list.append(Customer(name=customer["name"],
-                                                food=customer["food"]))
+        customer_class_obj_list.append(
+            Customer(
+                name=customer["name"],
+                food=customer["food"]
+            )
+        )
     for customer in customer_class_obj_list:
         CinemaBar.sell_product(customer, customer.food)
     hall = CinemaHall(hall_number)
