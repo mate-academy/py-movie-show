6ffacc5c9cdb57a5e79da6eb0ae6b3b54cac7d7d
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-01-22 12:46:16 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,21 +4,24 @@ from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list[dict[str, any]],
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str
-                 ) -> None:
-    customers_of_cinema = [Customer(instance_of_class["name"],
-                                    instance_of_class["food"]
-                                    )
-                           for instance_of_class in customers
-                           ]
-    for index_customer, customer in enumerate(customers_of_cinema):
+def cinema_visit(
+        customers: list[dict[str, any]],
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    customers_of_cinema = [
+        Customer(
+            instance_of_class["name"],
+            instance_of_class["food"]
+        )
+        for instance_of_class in customers
+    ]
+    for index, customer in enumerate(customers_of_cinema):
         cinema_bar = CinemaBar()
-        cinema_bar.sell_product(customers[index_customer]["food"],
-                                customer
-                                )
+        cinema_bar.sell_product(
+            customers[index]["food"], customer
+        )
     cinema = CinemaHall(hall_number)
     staff = Cleaner(cleaner)
     cinema.movie_session(movie, customers_of_cinema, staff)
