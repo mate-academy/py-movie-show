3709eaf6c5214c502587eb38480420218027d372
kuznetsb <kuznetsbog@gmail.com>
2023-02-08 18:02:53 +0200

    Correct variables names and annotations

--- app/main.py
+++ app/main.py
@@ -4,17 +4,17 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
+def cinema_visit(customers: list[dict],
                  hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+                 cleaning_staff: str,
+                 movie_name: str) -> None:
     clients = [Customer(customer["name"], customer["food"])
                for customer in customers]
     hall = CinemaHall(hall_number)
-    staff = Cleaner(cleaner)
+    staff = Cleaner(cleaning_staff)
     cinema_bar = CinemaBar()
     for client in clients:
         cinema_bar.sell_product(product=client.food, customer=client)
-    hall.movie_session(movie_name=movie,
+    hall.movie_session(movie_name=movie_name,
                        customers=clients,
                        cleaning_staff=staff)
