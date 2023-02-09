502cdb22f74dc3f34529d6fbfaeeaa4f1f8a7a27
Teliks <hapyto371@gmail.com>
2023-02-09 21:59:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
 ) -> None:
     bar = CinemaBar()
     hall = CinemaHall(hall_number)
