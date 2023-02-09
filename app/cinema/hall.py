fd38076810737692a1c5472f987c8e8081fb39f6
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-09 18:40:14 +0200

    2 commit

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
@@ -9,7 +8,7 @@ class CinemaHall:
 
     def movie_session(self,
                       movie_name: str,
-                      customers: [Customer],
+                      customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
