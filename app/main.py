f9d298f653ceb106e8669efe0bb5fb9249aecf39
Oleksandr Leha <legus96@gmail.com>
2023-02-09 19:50:25 +0200

    'Solution_with_new_changes'

--- app/main.py
+++ app/main.py
@@ -13,11 +13,12 @@ def cinema_visit(
     list_of_customers = []
     cleaner_instance = Cleaner(cleaner)
     cinema_instance = CinemaHall(hall_number)
+    bar_instance = CinemaBar()
     for customer_of_the_cinema_hall in customers:
         list_of_customers.append(Customer(customer_of_the_cinema_hall["name"],
                                           customer_of_the_cinema_hall["food"]))
     for customer in list_of_customers:
-        CinemaBar.sell_product(customer,
-                               customer.food)
+        bar_instance.sell_product(customer,
+                                  customer.food)
     cinema_instance.movie_session(movie_name,
                                   list_of_customers, cleaner_instance)
