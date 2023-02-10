de4199180cd0ffaa7203e2ffd68f42aecc95a652
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-10 13:39:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
+from typing import List, Dict
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list,
+def cinema_visit(customers: List[Dict],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
-    customers_list = [
-        Customer(name=customer.get("name"), food=customer.get("food"))
-        for customer in customers
-    ]
+    customers_list = [Customer(customer["name"], customer["food"])
+                      for customer in customers]
     hall = CinemaHall(hall_number)
     clean_stuff = Cleaner(cleaner)
     for customer in customers_list:
