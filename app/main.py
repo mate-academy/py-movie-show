c8ca170bb0a2d4313023865411bbc7466c7691f5
Eugene <eugene.lot77@gmail.com>
2023-02-10 12:41:28 +0200

    Chaged code for "good practice"

--- app/main.py
+++ app/main.py
@@ -4,19 +4,16 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaner: str,
-        movie: str
-) -> None:
-    clients = [
-        Customer(customer["name"], customer["food"]) for customer in customers
-    ]
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    clients = [Customer(customer["name"], customer["food"])
+               for customer in customers]
     [CinemaBar.sell_product(customer.food, customer) for customer in clients]
 
-    CinemaHall.movie_session(
-        self=CinemaHall(hall_number),
+    hall = CinemaHall(hall_number)
+    hall.movie_session(
         movie_name=movie,
         customers=clients,
         cleaning_staff=Cleaner(cleaner)
