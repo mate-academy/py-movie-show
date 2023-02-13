284dae1a8677bce7da22f2b53aa4990589e6bf15
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-13 17:24:03 +0200

    New solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,6 +4,4 @@ from app.people.customer import Customer
 class CinemaBar:
     @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
-        customer_check = (customer.name if isinstance(customer, Customer)
-                          else customer)
-        print(f"Cinema bar sold {product} to {customer_check}.")
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -4,20 +4,24 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     customer_instance = [
-        Customer(customer["name"], customer["food"]) for customer in customers
+        Customer(customer["name"], customer["food"])
+        for customer in customers
     ]
 
     cinemahall_instance = CinemaHall(hall_number)
     cinemabar_instance = CinemaBar()
     cleaner_instance = Cleaner(cleaner)
 
-    for customer in customers:
-        cinemabar_instance.sell_product(customer["name"], customer["food"])
+    for customer in customer_instance:
+        cinemabar_instance.sell_product(customer, customer.food)
+
     cinemahall_instance.movie_session(
         movie, customer_instance, cleaner_instance
     )
