24a10c18c4b65a14074d9e22b92008d737bcecaa
Roman Rega <romanrega1994@gmail.com>
2023-02-15 16:14:41 +0200

    Solution4

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -13,7 +13,7 @@ class CinemaHall:
             cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
-        for custom in customers:
-            custom.watch_movie(movie_name)
+        for customer in customers:
+            customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -7,12 +7,14 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(
         customers: list,
         hall_number: int,
-        cleaner: str, movie: str
+        cleaner: str,
+        movie: str
 ) -> None:
     customers_instances = [
         Customer(customer["name"],
                  customer["food"])
-        for customer in customers]
+        for customer in customers
+    ]
     cinema_bar_instances = CinemaBar()
     cinema_hall_instances = CinemaHall(hall_number)
     cleaner_inst_instances = Cleaner(cleaner)
