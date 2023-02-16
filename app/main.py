2b901d522f653bdbfeb2df104e1015c891305de0
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-16 11:32:43 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+from app.cinema.bar import CinemaBar
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
