c7e603fa1df54021088364b4e56b2878bc235aca
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-03-01 19:35:06 +0200

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,10 +4,7 @@ from app.people.customer import Customer
 
 
 class CinemaBar:
-    def __init__(self) -> None:
-        pass
 
     @staticmethod
     def sell_product(product: str, customer: Customer) -> None:
-        customer.food = product
         print(f"Cinema bar sold {product} to {customer.name}.")
