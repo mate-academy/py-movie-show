77489b88b2899123a6316b9c08081f2f990b23b7
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-06 11:58:17 +0200

    Select

--- app/people/customer.py
+++ app/people/customer.py
@@ -6,5 +6,5 @@ class Customer:
     def watch_movie(self, movie: str) -> None:
         print(f'{self.name} is watching \"{movie}\".')
 
-    def get(self, key: str) -> getattr:
+    def get(self, key: str) -> str:
         return getattr(self, key)
