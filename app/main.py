3f240ca438d5babac01fe2c5befa9195be89887e
Alexandr <olexandrzhezheria@gmail.com>
2023-03-13 21:30:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
+from __future__ import annotations
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    bar = CinemaBar()
-    hall = CinemaHall(hall_number)
-    cleaner = Cleaner(cleaner)
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    customer_list = [
+        Customer(customer["name"], customer["food"]) for customer in customers
+    ]
 
-    for customer_dict in customers:
-        customer = Customer(customer_dict['name'], customer_dict['food'])
-        bar.sell_product(customer, customer.food)
+    cinema_bar = CinemaBar()
+    hall = CinemaHall(number=hall_number)
+    staff = Cleaner(name=cleaner)
 
-    hall.movie_session(movie, [Customer(c['name'], c['food']) for c in customers], cleaner)
+    for customer in customer_list:
+        cinema_bar.sell_product(product=customer.food,
+                                customer=customer)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_list,
+        cleaning_staff=staff
+    )
