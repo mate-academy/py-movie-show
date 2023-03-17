6e09d261e8af7a26b6233b717941d318d05139ab
Volodymyr <elohimwz@gmail.com>
2023-03-17 11:50:50 +0200

    'Solutin#cinema'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    list_all_customers = [
+        Customer(customer["name"], customer["food"]) for customer in customers
+    ]
+    movie_hall = CinemaHall(hall_number)
+    clean_master = Cleaner(cleaner)
+    for cust in list_all_customers:
+        CinemaBar.sell_product(cust, cust.food)
+    movie_hall.movie_session(movie, list_all_customers, clean_master)
