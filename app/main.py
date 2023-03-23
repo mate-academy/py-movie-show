88b517c7871844050b989c7a1523ae037f100396
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-23 15:58:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,3 @@ def cinema_visit(customers: list, hall_number: int,
     customers_list = []
     for customer in customers:
         customers_list.append(Customer(customer["name"], customer["food"]))
-
-    cinema_hall = CinemaHall(hall_number)
-    cinema_bar = CinemaBar()
-    hall_cleaner = Cleaner(cleaner)
