9758aeff15c587037c5f6c88dfffaf33e3275a9d
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-26 12:50:46 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
@@ -10,7 +10,7 @@ def cinema_visit(customers: list, hall_number: int,
                       for person in customers]
 
     for customer in customers_list:
-        CinemaBar.sell_product(customer.name, customer.food)
+        CinemaBar.sell_product(customer, customer.food)
 
     cleaner_name = Cleaner(cleaner)
     hall = CinemaHall(hall_number)
