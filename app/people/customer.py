a5b7ea9648ac6b4c39a7638bb6d4f6ea137e999d
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-26 15:13:28 +0300

    Update customer.py

--- app/people/customer.py
+++ app/people/customer.py
@@ -9,7 +9,3 @@ class Customer:
 
     def watch_movie(self, movie: str) -> None:
         print(f'{self.name} is watching "{movie}".')
-
-
-#bob = Customer(name="Bob", food="popcorn")
-#bob.watch_movie(movie="Madagascar")
