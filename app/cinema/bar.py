52aec6b14fb0cbdd6118340133a72d14039e34e5
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-26 15:12:26 +0300

    Update bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -7,12 +7,3 @@ class CinemaBar:
     @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
-
-
-"""if __name__ == "__main__":
-    visitor = Customer("Alice", "Sprite")
-    CinemaBar.sell_product(visitor, visitor.food)
-
-cb = CinemaBar()
-customer = Customer("Bob", "popcorn")
-cb.sell_product(customer=customer, product=customer.food)"""
