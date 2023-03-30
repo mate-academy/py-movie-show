baac1829f3b9780db5a67842775f04d08fb87170
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-30 21:05:51 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-# flake8: --inline-quotes=single
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) \
