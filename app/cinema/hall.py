4477c5146c7f232bd04ea884df299e2db7bc99a4
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-30 14:22:57 +0300

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,10 +7,10 @@ class CinemaHall:
 
     def movie_session(self, movie_name: str, customers: list,
                       cleaning_staff: str) -> None:
-        print(f"'{movie_name}' started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            print(f"{Customer(name=customer.name, food=customer.food)} "
-                  f"is watching '{movie_name}'.")
-        print(f"'{movie_name}' ended.")
+            print(f'{Customer(name=customer.name, food=customer.food)} '
+                  f'is watching "{movie_name}".')
+        print(f'"{movie_name}" ended.')
         print(f"Cleaner {cleaning_staff} is cleaning hall number "
               f"{self.number}.")
