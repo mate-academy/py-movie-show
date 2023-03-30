43da37eb15ddd51c28868003759e7f6cb7ea3d46
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-30 20:42:04 +0300

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,7 +9,7 @@ class CinemaHall:
                       cleaning_staff: str) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            print(f'{Customer(name=customer.name, food=customer.food)} '
+            print(f'{Customer(customer.name, customer.food)} '
                   f'is watching "{movie_name}".')
         print(f'"{movie_name}" ended.')
         print(f"Cleaner {cleaning_staff} is cleaning hall number "
