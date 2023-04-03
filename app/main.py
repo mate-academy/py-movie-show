a639e21cfe0aaef9e72975dece46d322af30d8f1
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-03 22:00:19 +0300

    Create list for Customer instances

--- app/main.py
+++ app/main.py
@@ -7,13 +7,10 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
     cinema_bar = CinemaBar()
-    for man in customers:
-        cinema_bar.sell_product(Customer(man["name"], man["food"]),
-                                man["food"])
+    customers_list = [Customer(name=customer["name"], food=customer["food"])
+                      for customer in customers]
+    for man in customers_list:
+        cinema_bar.sell_product(man, man.food)
     hall = CinemaHall(hall_number)
     clean = Cleaner(cleaner)
-    hall.movie_session(
-        movie,
-        [Customer(name=customer["name"], food=customer["food"])
-         for customer in customers],
-        clean)
+    hall.movie_session(movie, customers_list, clean)
