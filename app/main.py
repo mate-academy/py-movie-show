f07f96038d9612ea3857a5bea1b87ef3ee87a1a1
Stanislav Bulanov <p99kab00@gmail.com>
2023-04-11 19:14:06 +0300

    Create initial implementation of the module

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customers = [
+        Customer(customer.get("name"), customer.get("food"))
+        for customer in customers
+    ]
+    cleaner = Cleaner(cleaner)
+    cinema_hall = CinemaHall(hall_number)
+
+    for customer in customers:
+        CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall.movie_session(movie, customers, cleaner)
