b9018574298690a78b288ce3483811790fe4ceea
Anton Hryhorenko <grygorenko23@gmail.com>
2023-04-28 20:46:54 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,14 +3,11 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
-Customer("Anton", "apple")
-Cleaner("Anton")
-
 
 def cinema_visit(
-        customers: list,
+        customers: list[Customer],
         hall_number: int,
-        cleaner: str,
+        cleaner: Cleaner(Customer),
         movie: str) -> None:
     for customer in customers:
         CinemaBar.sell_product(customer["food"], customer["name"])
