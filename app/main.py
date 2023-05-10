72f6ee04b7b23a6de1bf3829e09e87d36f49e0ea
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-10 06:18:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+import sys
+sys.path.append("D:/python_work/Mate Academy/py-cinema-visit/")
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+def cinema_visit(
+        customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+    all_visitors = [Customer(every_customer["name"], every_customer["food"])
+                    for every_customer in customers]
+    for visitor in all_visitors:
+        CinemaBar.sell_product(visitor.food, visitor)
+    current_cleaner = Cleaner(cleaner)
+    current_hall = CinemaHall(hall_number)
+    current_hall.movie_session(movie, all_visitors, current_cleaner)
