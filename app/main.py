d7b8b875259ac2a109bf877fea59b58ea23f2695
Artem Putsenko <artempucenko3@gmail.com>
2023-05-10 11:44:03 +0300

    'cinema-visit'

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ def cinema_visit(customers: str, hall_number: int,
                  cleaning_staff: str, movie_name: str) -> None:
     all_customers = []
 
-    bar = CinemaBar()
+    cinema_bar = CinemaBar()
     hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaning_staff)
 
     for customer_data in customers:
         customer = Customer(customer_data["name"], customer_data["food"])
-        bar.sell_product(customer, customer.food)
+        cinema_bar.sell_product(customer, customer.food)
         all_customers.append(customer)
 
     hall.movie_session(movie_name, all_customers, cleaner)
