81930219a0afe52b7e7a99bb3c733063c2344e2c
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-12 23:35:25 +0300

    refactoring code

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.append("D:/python_work/Mate Academy/py-cinema-visit/")
-
-
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
@@ -10,10 +6,10 @@ from app.people.customer import Customer
 
 def cinema_visit(
         customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    all_visitors = [Customer(every_customer["name"], every_customer["food"])
-                    for every_customer in customers]
-    for visitor in all_visitors:
+    visitors = [Customer(customer["name"], customer["food"])
+                for customer in customers]
+    for visitor in visitors:
         CinemaBar.sell_product(visitor.food, visitor)
-    current_cleaner = Cleaner(cleaner)
-    current_hall = CinemaHall(hall_number)
-    current_hall.movie_session(movie, all_visitors, current_cleaner)
+    cleaner = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+    hall.movie_session(movie, visitors, cleaner)
