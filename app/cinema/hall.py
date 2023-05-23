4a630c520e6967d08b8616f12ef58976b946ff6d
Vadym <seredavadimsv@gmail.com>
2023-05-23 18:40:15 +0300

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,14 +5,14 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list,
+    def movie_session(self, movie_name: str, customers: list[dict[str, str]],
                       cleaning_staff: Cleaner = None) -> None:
-        print(f'\"{movie_name}\" started in hall number {self.number}.')
+        print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f'\"{movie_name}\" ended.')
+        print(f'"{movie_name}" ended.')
 
         if cleaning_staff:
             cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -4,8 +4,11 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaning_staff: str,
-                 movie_name: str) -> None:
+def cinema_visit(customers: list[dict[str, str]],
+                 hall_number: int,
+                 cleaning_staff: str,
+                 movie_name: str
+                 ) -> None:
     cinema_bar = CinemaBar()
 
     for customer_data in customers:
