a05d99a8bba7ed965522c43608a441336b06a3d4
Oknametra <oknametra@gmail.com>
2023-05-23 20:27:51 +0200

    implemented cinema operations

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    new_customers = []
+    for customer in customers:
+        new_customers.append(
+            Customer(
+                customer["name"],
+                customer["food"]
+            )
+        )
+    ch = CinemaHall(hall_number)
+    cr = Cleaner(cleaner)
+    for customer in new_customers:
+        CinemaBar.sell_product(customer.food, customer)
+
+    ch.movie_session(
+        cleaning_staff=cr,
+        movie_name=movie,
+        customers=new_customers
+    )
