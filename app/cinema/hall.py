02b762f5894fd8813cb95a14d390a91f50b60535
Vadym <seredavadimsv@gmail.com>
2023-05-24 12:49:19 +0300

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,13 +1,17 @@
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
+from typing import Optional
 
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list[Customer],
-                      cleaning_staff: Cleaner = None) -> None:
+    def movie_session(
+            self, movie_name: str,
+            customers: list[Customer],
+            cleaning_staff: Optional[Cleaner] = None
+    ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list[Customer],
-                 hall_number: int,
-                 cleaning_staff: str,
-                 movie_name: str
-                 ) -> None:
+def cinema_visit(
+        customers: list[Customer],
+        hall_number: int,
+        cleaning_staff: str,
+        movie_name: str
+) -> None:
     cinema_bar = CinemaBar()
 
     for customer_data in customers:
