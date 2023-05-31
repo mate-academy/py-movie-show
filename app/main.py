881c2ccd1fedbc99337d49cf132d7d5255aca0c7
Oleksii Kiselov <crift34@gmail.com>
2023-05-30 23:25:52 -0400

    Implemented output of information about sales at the bar and cinema session

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
-# write your imports here
+from __future__ import annotations
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str, movie: str
+) -> None:
+
+    list_of_customers = []
+    for person_data in customers:
+        name = person_data["name"]
+        food = person_data["food"]
+        person = Customer(name=name, food=food)
+        list_of_customers.append(person)
+
+    for person in list_of_customers:
+        CinemaBar.sell_product(
+            product=person.food,
+            customer=person
+        )
+
+    cinema_hall = CinemaHall(number=hall_number)
+    current_cleaner = Cleaner(name=cleaner)
+    CinemaHall.movie_session(
+        self=cinema_hall,
+        movie_name=movie,
+        customers=list_of_customers,
+        cleaning_staff=current_cleaner
+    )
