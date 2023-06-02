000036e8fd0903ec1236fc813a6ba52251ae76b7
Mariia Horbova <gorbova21@gmail.com>
2023-06-02 13:00:58 +0300

    changed py-cinema-visit according to PR

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     customers_objects = []
