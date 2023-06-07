f470391d754de6b14b25d4cf1f86d2e7b59b4b66
Vlad Larin <lordgadyka71@gmail.com>
2023-06-07 13:51:29 +0300

    solution3

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(
-        customers: list,
+        customers: list[dict[str, str]],
         hall_number: int,
         cleaner: str,
         movie: str
