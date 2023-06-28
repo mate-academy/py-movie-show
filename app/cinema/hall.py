f4e6bee638eac97a4eb9a7be16905cbefbb6bc7c
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-06-28 20:50:53 +0300

    import_solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,5 +1,5 @@
-from people.customer import Customer
-from people.cinema_staff import Cleaner
+from ..people.customer import Customer
+from ..people.cinema_staff import Cleaner
 
 
 class CinemaHall:
@@ -7,14 +7,18 @@ class CinemaHall:
 
         self.number = number
 
-    def movie_session(self,
-                      movie_name: str,
-                      customers: list,
-                      cleaning_staff: str
-                      ) -> None:
-        print(f"{movie_name} started in hall number {self.number}.")
-        for customer in customers:
-            Customer(customer["name"],
-                     customer["food"]).watch_movie(movie_name)
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list,
+            cleaning_staff: str
+    ) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
+        for cust in customers:
+            Customer(cust.name, cust.food).watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        Cleaner(cleaning_staff).clean_hall(self.number)
+
+        if type(cleaning_staff) is Cleaner:
+            Cleaner(cleaning_staff.name).clean_hall(self.number)
+        else:
+            Cleaner(cleaning_staff).clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list,
@@ -8,9 +9,21 @@ def cinema_visit(customers: list,
                  cleaner: str,
                  movie: str
                  ) -> None:
-
+    list_of_customer = []
     for cust in customers:
         customer = Customer(cust["name"], cust["food"])
+        list_of_customer.append(customer)
         CinemaBar.sell_product(product=cust["food"], customer=customer)
 
-    CinemaHall(hall_number).movie_session(movie, customers, cleaner)
+    if type(cleaner) is Cleaner:
+        CinemaHall(hall_number).movie_session(
+            movie,
+            list_of_customer,
+            cleaner.name
+        )
+    else:
+        CinemaHall(hall_number).movie_session(
+            movie,
+            list_of_customer,
+            cleaner
+        )
