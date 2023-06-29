1b5cd54aa8190c5782c9f1fdb3919ac998a3a67b
Vladyslav Shepilov <shepsterya@gmail.com>
2023-06-29 12:31:41 +0300

    resolved, probably mistake in tests/test_cleaner.py in test cleaner hall method in 27th string

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    hall_ = CinemaHall(hall_number)
+    bar_ = CinemaBar()
+    cleaner_ = Cleaner(cleaner)
+    customers_list_ = []
+    for person in customers:
+        customers_list_.append(Customer(person["name"], person["food"]))
+    for person in customers_list_:
+        bar_.sell_product(person, person.food)
+    hall_.movie_session(movie, customers_list_, cleaner_)
