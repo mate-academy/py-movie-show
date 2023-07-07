99feec198201dc3ed8f85d2764f7df0a932db449
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-07 15:16:38 +0300

    Added dict typehint for customers list in cinema_visit

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-        customers: list[Customer],
+        customers: list[dict],
         hall_number: int,
         cleaner: str,
         movie: str
@@ -16,6 +16,7 @@ def cinema_visit(
                for elem in customers]
     for viewer in viewers:
         CinemaBar.sell_product(viewer.food, viewer)
+
     current_cleaner = Cleaner(cleaner)
     hall = CinemaHall(hall_number)
     hall.movie_session(
