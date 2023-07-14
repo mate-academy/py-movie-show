f36aa081bd63c4aa743e6764a60a407ba8d7c2d7
Alla Kuksa <alla.kuksa92@gmail.com>
2023-07-14 08:20:57 +0300

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,10 +1,7 @@
-from typing import Any
-
-
 from app.people.customer import Customer
 
 
 class CinemaBar:
     @staticmethod
-    def sell_product(product: str, customer: Customer) -> Any:
+    def sell_product(product: str, customer: Customer) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
@@ -13,7 +10,7 @@ class CinemaHall:
     def movie_session(self,
                       movie_name: str,
                       customers: list[Customer],
-                      cleaning_staff: Cleaner) -> Any:
+                      cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
