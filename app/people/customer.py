c655031a385c9b13db035770f17f1765d61b0637
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-14 18:45:01 +0300

    Update customer.py

--- app/people/customer.py
+++ app/people/customer.py
@@ -2,10 +2,9 @@ class Customer:
     def __init__(self,
                  name: str,
                  food: str
-                ) -> None:
+                 ) -> None:
         self.name = name
         self.food = food
-    
     def watch_movie(self,
                     movie: str
                     ) -> None:
