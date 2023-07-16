0660f19cc8b7523f31fcaefa36460d0bc874f4bb
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-16 10:21:52 +0300

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,7 +5,7 @@ from app.people.cinema_staff import Cleaner
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
-        
+
     def movie_session(self,
                       movie_name: str,
                       customers: list[Customer],
