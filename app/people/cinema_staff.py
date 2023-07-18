d7f30c85489e52b6dd5e3a4a542d207631643fc2
Olena Sapohova <olenka.sapogova@gmail.com>
2023-07-19 00:24:55 +0300

    'Solution'

---
+++ app/people/cinema_staff.py
@@ -0,0 +1,9 @@
+class Cleaner:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def clean_hall(self, hall_number: int) -> None:
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
+
+# anna = Cleaner(name="Anna")
+# anna.clean_hall(hall_number=5)
