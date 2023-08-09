8767112b42520314a5548ee67ba1622695a60763
oreny <orenycdota@gmail.com>
2023-08-09 15:21:56 +0300

    created new files according to task in Readme. Every file has his own Class and appropriate method. Tests are correct.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    customers_instance = []
+    for element in customers:
+        customers_instance.append(Customer(
+            name=element.get("name"),
+            food=element.get("food")
+        ))
+    hall_instance = CinemaHall(
+        number=hall_number
+    )
+    cleaner_instance = Cleaner(
+        name=cleaner
+    )
+    bar_instance = CinemaBar()
+
+    for customer in customers_instance:
+        bar_instance.sell_product(customer.food, customer)
+
+    hall_instance.movie_session(
+        movie_name=movie,
+        customers=customers_instance,
+        cleaning_staff=cleaner_instance
+    )
