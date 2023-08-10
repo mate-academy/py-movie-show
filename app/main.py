dd7bfd23b8a6c60ac1639709bf6a63177ed2d193
oreny <orenycdota@gmail.com>
2023-08-10 12:43:47 +0300

    Renamed element in customer, cuteomers_instance into customers_list to make code more readable

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
-    customers_instance = [
+    customers_list = [
         Customer(
-            name=element.get("name"),
-            food=element.get("food")
-        ) for element in customers
+            name=customer.get("name"),
+            food=customer.get("food")
+        ) for customer in customers
     ]
     hall_instance = CinemaHall(
         number=hall_number
@@ -22,11 +22,11 @@ def cinema_visit(customers: list,
     )
     bar_instance = CinemaBar()
 
-    for customer in customers_instance:
+    for customer in customers_list:
         bar_instance.sell_product(customer.food, customer)
 
     hall_instance.movie_session(
         movie_name=movie,
-        customers=customers_instance,
+        customers=customers_list,
         cleaning_staff=cleaner_instance
     )
