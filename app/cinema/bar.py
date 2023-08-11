a2551cd77921ecb444621526b942b130ee0e5e2b
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-11 10:45:21 +0200

    Fixed tests and string output in bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,4 +4,4 @@ from app.people.customer import Customer
 class CinemaBar:
     @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
-        print(f"cinema bar sold {product} to {customer.name}.")
+        print(f"Cinema bar sold {product} to {customer.name}.")
