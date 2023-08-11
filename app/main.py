f6a718031ea7200ac6d1eaa713febc7aa7445337
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-11 10:06:31 +0300

    cinema_structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-# write your imports here
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
 
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str):
+    cinema_hall = CinemaHall(number=hall_number)
+    cleaner = Cleaner(cleaner_name)
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customer_list = []
+    for customer data in customers:
+        customer = Customer(customer_data["name"], customer_data.get("food"))
+        customer_list.append(customer)
+        if customer.food is not None:
+            CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall.movie_session(movie_name = movie_name,
+                              customers = customers_list,
+                              cleaning_staff = cleaner)
---
+++ app/people/customer.py
@@ -0,0 +1,8 @@
+class Customer
+
+    def __init__(self, name: str, food: str | None) -> None:
+        self.name: str = name
+        self.food: str| None = food
+
+    def watch_movie(self, movie: str) -> None:
+        print(f'{self.name} is watching "{movie}".')
