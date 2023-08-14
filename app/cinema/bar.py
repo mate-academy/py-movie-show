fc348c1400a24a599008eadec05661df96625177
Max Katkalov <makskatkalov@gmail.com>
2023-08-14 12:43:14 +0300

    TODO and come code deleted

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,8 +4,4 @@ from app.people.customer import Customer
 class CinemaBar:
     @staticmethod
     def sell_product(product: str, customer: Customer) -> None:
-        """
-        TODO
-        add "__future__" and change to Customer class
-        """
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,16 +9,9 @@ class CinemaHall:
     def movie_session(
         self,
         movie_name: str,
-        customers: list["Customer"],
+        customers: list[Customer],
         cleaning_staff: Cleaner,
     ) -> None:
-        """ " TODO:
-        that takes movie_name, customers - list of a customers
-        (Customer instances), cleaning_staff -
-        cleaner (Cleaner instance).
-        This method prints about movie start, calls
-        customers method watch_movie, prints about
-        movie end, calls cleaner method clean_hall."""
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
