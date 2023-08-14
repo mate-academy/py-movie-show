5faae921896d954ee8962f81ade28fa8cc343561
Ruslan <rusipbox@gmail.com>
2023-08-14 12:29:36 +0300

    Test(py-cinema-vist)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int or str,
+        cleaning_staff: str,
+        movie_name: str or int
+) -> None:
+    cinema_hall = CinemaHall(hall_number)
+    cinema_bar = CinemaBar()
+    cleaner_instance = Cleaner(cleaning_staff)
+
+    customer_instances = [
+        Customer(
+            cust["name"],
+            cust["food"]
+        )
+        for cust in customers
+    ]
+
+    for cust_instance in customer_instances:
+        cinema_bar.sell_product(cust_instance, cust_instance.food)
+
+    cinema_hall.movie_session(
+        movie_name,
+        customer_instances,
+        cleaner_instance
+    )
