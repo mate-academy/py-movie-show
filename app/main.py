4e58e0cd899a457cce448be4ffa54e75addbab6b
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-15 01:34:02 +0300

    cinema_structure_correct_v6

--- app/main.py
+++ app/main.py
@@ -1,25 +1,18 @@
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
+    customers: list[dict], hall_number: int, cleaner: str, movie: str
+) -> None:
+    list_of_customers = [
+        Customer(customer["name"], customer["food"]) for customer in customers
+    ]
+    for customer in list_of_customers:
+        CinemaBar.sell_product(customer, customer.food)
 
-        customers: list[dict],
-        hall_number: int,
-        cleaner_name: str,
-        movie_name: str) -> None:
-    cinema_hall = CinemaHall(number=hall_number)
-    cleaner = Cleaner(cleaner_name)
-
-    customers_list = []
-    for customer_data in customers:
-        customer = Customer(customer_data["name"], customer_data.get("food"))
-        customers_list.append(customer)
-        if customer.food is not None:
-            CinemaBar.sell_product(customer, customer.food)
-
-    cinema_hall.movie_session(movie_name=movie_name,
-                              customers=customers_list,
-                              cleaning_staff=cleaner)
+    cinema_hall = CinemaHall(hall_number)
+    cleaning_stuff = Cleaner(cleaner)
+    cinema_hall.movie_session(movie, list_of_customers, cleaning_stuff)
