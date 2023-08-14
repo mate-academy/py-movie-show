35229d6c351fa972c771e8511f429d4066275171
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-08-14 17:40:58 +0300

    Solution

---
+++ app/cinema/__init__.py
@@ -0,0 +1,18 @@
+class CinemaBar:
+    pass
+
+
+class CinemaHall:
+    pass
+
+
+class Cleaner:
+    pass
+
+
+def cleaner() -> None:
+    return None
+
+
+def cinema_staff() -> None:
+    return None
---
+++ app/people/__init__.py
@@ -0,0 +1,14 @@
+class Customer:
+    pass
+
+
+def watch_movie() -> None:
+    return None
+
+
+def clean_hall() -> None:
+    return None
+
+
+class Cleaner:
+    pass
