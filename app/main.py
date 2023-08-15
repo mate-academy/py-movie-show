31fa87f5d3a43726134d343d29ef15b7e92dc814
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-15 15:59:04 +0300

    cinema_structure_correct_v6

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-    customers: list[dict], hall_number: int, cleaner: str, movie: str
-) -> None:
+                customers: list[dict],
+                hall_number: int,
+                cleaner: str,
+                movie: str
+                ) -> None:
     list_of_customers = [
         Customer(customer["name"], customer["food"]) for customer in customers
     ]
