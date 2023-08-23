d547063e416ff4eae2cff8fef30701454b178541
Seniv Volodymyr <01156240@pw.edu.pl>
2023-08-23 13:43:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     cinema_hall = CinemaHall(hall_number)
     cleaner_name = Cleaner(cleaner)
     people = []
