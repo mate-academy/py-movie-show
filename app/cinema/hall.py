dfafdf278871c3d010bb77a6db6da89c6ab0e37e
Nikolay Krasulin <krasulinphotograph@example.com>
2023-08-28 16:00:52 +0300

    solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -2,7 +2,12 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list, cleaning_staff: object) -> None:
+    def movie_session(
+            self, movie_name: str,
+            customers: list,
+            cleaning_staff: object
+    ) -> None:
+
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for person in customers:
--- app/main.py
+++ app/main.py
@@ -5,18 +5,24 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    customers_list_instance = []
-    for person in customers:
-        customers_person_instance = Customer(person.get("name"), person.get("food"))
-        customers_list_instance.append(customers_person_instance)
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+
+    custom_list_inst = []
+    for pers in customers:
+        custom_pers_inst = Customer(pers.get("name"), pers.get("food"))
+        custom_list_inst.append(custom_pers_inst)
 
     c_hall_inst = CinemaHall(hall_number)
     bar = CinemaBar()
 
     cleaner_instance = Cleaner(cleaner)
 
-    for person in customers_list_instance:
-        bar.sell_product(person, person.food)
+    for pers in custom_list_inst:
+        bar.sell_product(pers, pers.food)
 
-    c_hall_inst.movie_session(movie, customers_list_instance, cleaner_instance)
+    c_hall_inst.movie_session(movie, custom_list_inst, cleaner_instance)
