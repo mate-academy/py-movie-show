ab30aa918c0b7f7f6e277e34e7df2b9dae735216
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2023-08-30 21:24:52 +0300

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -14,12 +14,12 @@ class CinemaHall:
             movie_name: str,
             customers: list[Customer],
             cleaning_staff: list[Cleaner]
-    ) -> str:
-        print(f"{movie_name} started in hall number {self.number}")
+    ) -> None:
 
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f"'{movie_name}' ended")
+        print(f'"{movie_name}" ended.')
 
         cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,26 +1,20 @@
-from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.cinema.bar import CinemaBar
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
-
-def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaning_staff: str,
-        movie: str
-) -> None:
-
+def cinema_visit(customers, hall_number, cleaner, movie):
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
-    cleaner = Cleaner(cleaning_staff)
+    cleaning_staff = Cleaner(cleaner)
 
-    for custom in customers:
-        customer = Customer(name=custom["name"], food=custom["food"])
+    for customer_data in customers:
+        customer = Customer(name=customer_data["name"], food=customer_data["food"])
         cinema_bar.sell_product(customer, customer.food)
 
     cinema_hall.movie_session(
         movie_name=movie,
-        customers=[Customer[i] for i in customers],
-        cleaning_staff=cleaner
+        customers=[Customer(**i) for i in customers],
+        cleaning_staff=Cleaner(name=cleaner)
     )
+    
