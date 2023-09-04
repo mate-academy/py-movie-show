3cfa71b7310c26a18bd2081328deb69f902b8a43
Serhii Budko <sergeybudko.kr@gmail.com>
2023-09-04 20:47:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-        movie: str,
         customers: list[dict],
         hall_number: int,
-        cleaner: str
+        cleaner: str,
+        movie: str,
 ) -> None:
     cinema_bar = CinemaBar()
     hall = CinemaHall(hall_number)
