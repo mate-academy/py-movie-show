543048a134bc63c43991ae17fe05aa84d92daafd
Mariia Krushynska <winter27m@gmail.com>
2023-09-11 19:52:47 +0300

    style: write each argument on a new line

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list[dict],
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: list[dict],
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
 
     visitors = [Customer(name=customer["name"], food=customer["food"])
                 for customer in customers]
