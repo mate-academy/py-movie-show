9487cbf02a234e1013be728b8a92ec33309ec60c
Dmytro Tatarinov <nsd7night@gmail.com>
2023-09-13 19:45:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
@@ -5,23 +6,24 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-        customers: list,
+        customers: List[Dict],
         hall_number: int,
         cleaner: str,
         movie: str
 ) -> None:
     customers_instances = []
     for customer_dict in customers:
-        name = customer_dict.get("name")
-        food = customer_dict.get("food")
-        customer = Customer(name, food)
+        customer = Customer(
+            name=customer_dict.get("name"),
+            food=customer_dict.get("food")
+        )
         customers_instances.append(customer)
 
-    hall_instance = CinemaHall(hall_number)
-    bar_instance = CinemaBar()
-    cleaner_person = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+    cinema_bar = CinemaBar()
+    cleaner = Cleaner(cleaner)
 
     for customer in customers_instances:
-        bar_instance.sell_product(customer, customer.food)
+        cinema_bar.sell_product(customer, customer.food)
 
-    hall_instance.movie_session(movie, customers_instances, cleaner_person)
+    hall.movie_session(movie, customers_instances, cleaner)
