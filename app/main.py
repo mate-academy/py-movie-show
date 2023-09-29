7739a4b64087245d5df24043255a16004b1dfb0f
Your_Name Your_Surname <your@email>
2023-09-29 20:40:54 +0300

    need help for this task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-# write your imports here
+from app.people import cinema_staff
+from app.people import customer
+from app.cinema import bar
+from app.cinema import hall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+    cb = bar.CinemaBar
+    ch = hall.CinemaHall(hall_number)
+    list_customer = [customer.Customer(custom["name"], custom["food"])
+                     for custom in customers]
+    clean = cinema_staff.Cleaner(cleaner)
+    for value in list_customer:
+        cb.sell_product(value.name, value.food)
+    ch.movie_session(movie, list_customer, clean)
