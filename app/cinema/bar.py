4fb3851b0609ed0fb2d53626a9514bbf76d6a489
Your_Name Your_Surname <your@email>
2023-09-30 10:10:40 +0300

    fix bag in program

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,7 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
     @staticmethod
-    def sell_product(custom: str, product: str) -> None:
-        print(f"Cinema bar sold {product} to {custom}.")
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from app.people import cinema_staff
-from app.people import customer
-from app.cinema import bar
-from app.cinema import hall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    cb = bar.CinemaBar
-    ch = hall.CinemaHall(hall_number)
-    list_customer = [customer.Customer(custom["name"], custom["food"])
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str
+                 ) -> None:
+    cb = CinemaBar
+    ch = CinemaHall(hall_number)
+    list_customer = [Customer(custom["name"], custom["food"])
                      for custom in customers]
-    clean = cinema_staff.Cleaner(cleaner)
+    clean = Cleaner(cleaner)
     for value in list_customer:
-        cb.sell_product(value.name, value.food)
+        cb.sell_product(value, value.food)
     ch.movie_session(movie, list_customer, clean)
