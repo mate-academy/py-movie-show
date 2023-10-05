43e5cd49aee470921d214cb9c244749685da17de
Kostia <kostia3507@gmail.com>
2023-10-05 18:11:44 +0300

    All packages done

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,4 +6,11 @@ class CinemaHall:
         self.number = number
 
     def movie_session(self, movie_name: str, customers: List["Customer"], cleaning_staff: "Cleaner"):
+        print(f"\"{movie_name}\" started in hall {self.number}")
 
+        for customer in customers:
+            customer.watch_movie()
+
+        print(f"\"{movie_name}\" ended")
+
+        cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-# write your imports here
-
-
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     # write you code here
     pass
