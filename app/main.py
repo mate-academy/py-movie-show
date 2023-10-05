58d32b569f22319fa75f477dc56765ff4d69c739
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-10-05 10:21:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
 
-    customers = [Customer(customer["name"], customer["food"])
+    customers = [Customer(customer.get("name"), customer.get("food"))
                  for customer in customers]
     cinema_hall = CinemaHall(hall_number)
     cinema_cleaner = Cleaner(cleaner)
