f9343954b40ddbd6994e5b4f5e76b6c142dbc2a0
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-10-05 12:57:04 +0300

    Solution_Final

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,12 +1,17 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list,
-                      cleaning_staff: list) -> None:
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list[Customer],
+                      cleaning_staff: Cleaner) -> None:
         print(f'\"{movie_name}\" started in hall number {self.number}.')
-        for movie in customers:
-            movie.watch_movie(movie_name)
+        for customer in customers:
+            customer.watch_movie(movie_name)
         print(f'\"{movie_name}\" ended.')
         cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -4,15 +4,21 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str) -> None:
-    hall1 = CinemaHall(hall_number)
-    cleaner1 = Cleaner(cleaner)
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner_name: str,
+                 movie_title: str) -> None:
+    hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner_name)
     cinema_bar = CinemaBar()
-    customers_resutl = []
-    for customer_data in customers:
-        customer = Customer(name=customer_data["name"],
-                            food=customer_data["food"])
-        customers_resutl.append(customer)
+    visitors = []
+
+    for customer_info in customers:
+        customer = Customer(
+            name=customer_info["name"],
+            food=customer_info["food"]
+        )
+        visitors.append(customer)
         cinema_bar.sell_product(customer.food, customer)
-    hall1.movie_session(movie, customers_resutl, cleaner1)
+
+    hall.movie_session(movie_title, visitors, cleaner)
