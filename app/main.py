c8887570e6a48548de16eaa13c6c07bc73e7d87f
Valeriia Danyliuk <bladeryz@gmail.com>
2023-10-06 19:33:28 +0300

    Added all the necessary modules and their methods. Then, successfully imported them into the main file and implemented the 'cinema_visit' method!

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str) -> None:
+
+    updated_customers = []
+
+    for customer in customers:
+        new_customer = Customer(customer.get("name"), customer.get("food"))
+
+        updated_customers.append(new_customer)
+
+    for customer in updated_customers:
+        CinemaBar.sell_product(customer, customer.food)
+
+    updated_cleaner = Cleaner(cleaner)
+
+    CinemaHall(hall_number).movie_session(movie,
+                                          updated_customers,
+                                          updated_cleaner)
