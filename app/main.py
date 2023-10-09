ef5f99345baaf699e915d9c8e1cef543c6bf5a82
Lytillis <chemurchem@gmail.com>
2023-10-09 08:43:14 +0300

    Fixed flake8 issues

--- app/main.py
+++ app/main.py
@@ -4,14 +4,17 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     customer_instances = []
     for customer in customers:
         customer_instance = Customer(customer["name"], customer["food"])
         customer_instances.append(customer_instance)
     hall = CinemaHall(hall_number)
-    bar = CinemaBar()
+    cinema_bar = CinemaBar()
     cleaner_instance = Cleaner(cleaner)
     for customer in customer_instances:
-        bar.sell_product(customer.food, customer)
+        cinema_bar.sell_product(customer.food, customer)
     hall.movie_session(movie, customer_instances, cleaner_instance)
