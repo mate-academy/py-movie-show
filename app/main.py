c6c0cd39f7d153c883ddb99821a8eff7d5ef0f65
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-09 15:15:18 +0300

    Chenged cinema_visit

--- app/main.py
+++ app/main.py
@@ -8,11 +8,10 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
-    cinema_bar = CinemaBar()
     customers_list = [Customer(customer["name"], customer["food"])
                       for customer in customers]
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
     for customer in customers_list:
-        cinema_bar.sell_product(customer.food, customer)
+        CinemaBar.sell_product(customer.food, customer)
     CinemaHall.movie_session(cinema_hall, movie, customers_list, cleaner)
