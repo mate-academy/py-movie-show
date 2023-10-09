5ba6ad0774a7a88aae752cb0e38da25d19a3e956
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-09 21:13:57 +0300

    Made a better code style

--- app/main.py
+++ app/main.py
@@ -5,24 +5,18 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaner: str,
-        movie: str
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
 ) -> None:
-    normalized_customers = []
-
-    for customer in customers:
-        normalized_customers.append(
-            Customer(
-                customer.get("name"),
-                customer.get("food")
-            )
-        )
-
     cinema_hall = CinemaHall(hall_number)
-
     normalized_cleaner = Cleaner(cleaner)
+
+    normalized_customers = [
+        Customer(customer["name"], customer["food"])
+        for customer in customers
+    ]
 
     for customer in normalized_customers:
         CinemaBar.sell_product(customer.food, customer)
