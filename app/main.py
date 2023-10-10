a27808567ff63490ebd53a5e09db00f2309e859b
Eugene Rabenko <qwer09846@gmail.com>
2023-10-10 11:49:14 +0300

    Corrected_main.py

--- app/main.py
+++ app/main.py
@@ -10,11 +10,10 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-    cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
-    customers_value = [Customer(customer.get("name"), customer.get("food"))
-                       for customer in customers]
-    for customer in customers_value:
-        cinema_bar.sell_product(customer.food, customer)
-    cinema_hall.movie_session(movie, customers_value, cleaner)
+    customers = [Customer(customer.get("name"), customer.get("food"))
+                 for customer in customers]
+    for customer in customers:
+        CinemaBar.sell_product(customer.food, customer)
+    cinema_hall.movie_session(movie, customers, cleaner)
