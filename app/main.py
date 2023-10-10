83f2ad6fcd7a34e8eb1e393f86ec9ea5926ffb08
OlehShumov <shumovoleh@ukr.net>
2023-10-10 20:53:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,22 +8,15 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
-    cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
 
-    for customer_info in customers:
-        customer = Customer(
-            name=customer_info["name"],
-            food=customer_info["food"]
-        )
-        cinema_bar.sell_product(customer, customer.food)
+    customer_list = [
+        Customer(name=customer_info["name"], food=customer_info["food"])
+        for customer_info in customers
+    ]
 
-    cinema_hall.movie_session(
-        movie,
-        [Customer(
-            name=customer_info["name"],
-            food=customer_info["food"]
-        ) for customer_info in customers],
-        cleaner
-    )
+    for customer in customer_list:
+        CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall.movie_session(movie, customer_list, cleaner)
