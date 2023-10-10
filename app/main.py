5f59d69dd66368da6932cb9a0ae899318d047f1c
Severyn Krok <sevkrok@gmail.com>
2023-10-10 15:05:43 +0300

    fix formatting

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str
-                 ) -> None:
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     cleaner = Cleaner(cleaner)
     hall_number = CinemaHall(hall_number)
 
