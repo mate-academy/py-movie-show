3319bcd6226a48bd8242a4258232df3f48416629
Yuliia Dudych <juliadudych@gmail.com>
2023-10-17 21:37:31 +0300

    Add logic to main to reflect all actions in the cinema

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list[dict] | list[Customer],
+        hall_number: int,
+        cleaning_staff: str,
+        movie_name: str
+) -> None:
+
+    for person in customers:
+        if isinstance(person, dict):
+            food, name = person["food"], person["name"]
+        if isinstance(person, Customer):
+            food, name = person.food, person.name
+        CinemaBar.sell_product(food, name)
+
+    hall = CinemaHall(hall_number)
+    hall.movie_session(movie_name, customers, Cleaner(cleaning_staff))
