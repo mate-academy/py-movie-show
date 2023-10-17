f6ef5ab3bc8a5edf57b18c6cedfdaa77893c8f3d
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2023-10-17 13:58:22 +0300

    'Removed_marks'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,7 +8,7 @@ class CinemaHall:
     def movie_session(
             self, movie_name: str,
             customers: list,
-            cleaning_staff: "Cleaner"
+            cleaning_staff: Cleaner
     ) -> str:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
