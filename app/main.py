89ca79a0df9d83d071a72716775fa48732575536
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-19 23:14:59 +0300

    Update main.py
    add import

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
