d75c3558019bd6e3b9112ea064f361bc25939830
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-19 23:01:02 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 # write your imports here
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner_name)
+
+    for customer_data in customers:
+        custoner = Customer(name=customer_data["name"], food=customer_data["food"])
+        cinema_bar.sell_product(customer. customer.food)
+
+    cinema_hall.mavie_sessions(
+        movie_name,
+        [Customer(name=customer_data["name"], food=customer_data["food"]) for customer_data in customers],
+        cleaner
+    )
+
+customers = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
+hall_number = 5
+cleaner_name = "Anna"
+movie = "Madagascar"
+cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
