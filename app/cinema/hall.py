89ea27e6a3e889ad989df0363d1a266c295b3d17
Yevhen Kunieiev <barelosfreda@gmail.com>
2023-10-20 20:22:47 +0300

    Py_cinema_visit attempt_5 (fixed flake8)

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,7 +5,10 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list, cleaning_staff: Cleaner) -> None:
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list,
+                      cleaning_staff: Cleaner) -> None:
         print(f'\"{movie_name}\" started in hall number {self.number}.')
         [customer.watch_movie(movie_name) for customer in customers]
         print(f'\"{movie_name}\" ended.')
--- app/main.py
+++ app/main.py
@@ -4,11 +4,14 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaning_staff: str, movie_name: str) -> None:
+def cinema_visit(customers: list, hall_number: int,
+                 cleaning_staff: str, movie_name: str) -> None:
     cb = CinemaBar()
     customer_instances = []
     for customer in customers:
         new_customer = Customer(customer["name"], customer["food"])
         customer_instances.append(new_customer)
         cb.sell_product(customer["food"], new_customer)
-    CinemaHall(hall_number).movie_session(movie_name, customer_instances, Cleaner(cleaning_staff))
+    CinemaHall(hall_number).movie_session(movie_name,
+                                          customer_instances,
+                                          Cleaner(cleaning_staff))
