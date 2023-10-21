1ae2d2c7da988bcd2c1239d7398a970c6d2233e6
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-21 21:08:43 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,16 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
-
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list, hall_number: int, cleaner_name: str, movie_name: str) -> None:
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner_name)
 
     for customer_data in customers:
-        custoner = Customer(name=customer_data["name"], food=customer_data["food"])
-        cinema_bar.sell_product(customer. customer.food)
+        customer = Customer(name=customer_data["name"], food=customer_data["food"])
+        cinema_bar.sell_product(customer, customer.food)
 
-    cinema_hall.mavie_sessions(
+    cinema_hall.movie_session(
         movie_name,
         [Customer(name=customer_data["name"], food=customer_data["food"]) for customer_data in customers],
         cleaner
@@ -25,5 +24,5 @@ customers = [
 ]
 hall_number = 5
 cleaner_name = "Anna"
-movie = "Madagascar"
-cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
+movie_name = "Madagascar"
+cinema_visit(customers, hall_number, cleaner_name, movie_name)
