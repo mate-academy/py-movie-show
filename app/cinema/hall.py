d3edeadcf52f113c4b9917283e7d52798eaf2562
Yuliia Dudych <juliadudych@gmail.com>
2023-10-21 14:15:56 +0300

    Remove list[dict] and related code from customers type annotation

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,19 +9,14 @@ class CinemaHall:
     def movie_session(
             self,
             movie_name: str,
-            customers: list[customer.Customer] | list[dict],
+            customers: list[customer.Customer],
             cleaning_staff: cinema_staff.Cleaner
     ) -> None:
 
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for person in customers:
-            if isinstance(person, dict):
-                name, food = person["name"], person["food"]
-            if isinstance(person, customer.Customer):
-                name, food = person.name, person.food
-
-            new_customer = customer.Customer(name, food)
+            new_customer = customer.Customer(person.name, person.food)
             new_customer.watch_movie(movie_name)
 
         print(f'"{movie_name}" ended.')
