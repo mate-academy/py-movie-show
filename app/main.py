cd25db7fd5f7a070c2b95b89b45b02fd8d5d6dc9
Yuliia Valko <yuliia.valko1@gmail.com>
2023-10-30 20:56:58 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from typing import List, Dict
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-from typing import List, Dict
 
 
 def cinema_visit(
@@ -11,20 +12,19 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-    persons_inst = []
-    for customer in customers:
-        person = Customer(name=customer["name"], food=customer["food"])
-        persons_inst.append(person)
-
+    people = [
+        Customer(name=customer["name"],
+                 food=customer["food"]) for customer in customers
+    ]
     cinema_hall = CinemaHall(number=hall_number)
     cinema_bar = CinemaBar()
     cleaner = Cleaner(name=cleaner)
 
-    for person in persons_inst:
+    for person in people:
         cinema_bar.sell_product(person, person.food)
 
     cinema_hall.movie_session(
         movie_name=movie,
-        customers=persons_inst,
+        customers=people,
         cleaning_staff=cleaner
     )
