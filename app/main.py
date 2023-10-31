3f8e75f8b5e29865e15f74d1d88639dd62849733
Yuliia Valko <yuliia.valko1@gmail.com>
2023-10-31 19:33:04 +0200

    'Solution_4'

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
