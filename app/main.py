9a59431afb61835882a26f3803f9cc7ff91b9b93
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-11-03 12:49:06 +0200

    deleted implementation of methods

--- app/main.py
+++ app/main.py
@@ -17,13 +17,3 @@ def cinema_visit(
         people.append(person)
 
     CinemaHall(number).movie_session(movie_name, people, Cleaner(cleaner))
-
-
-customers_list = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-cinema_visit(customers=customers_list, number=5, cleaner="Anna", movie_name="Madagascar")
