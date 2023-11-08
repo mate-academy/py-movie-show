826b4f293f6649bee1f07276ecd7d5f88ae6cccc
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-08 20:00:45 +0100

    Delete selftest code

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -6,8 +6,3 @@ class Cleaner:
 
     def clean_hall(self, hall_number: int) -> None:
         print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
-
-
-if __name__ == "__main__":
-    c = Cleaner("Coco")
-    c.clean_hall(5)
