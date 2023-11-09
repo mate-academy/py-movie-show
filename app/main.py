f4c8024333dc00f4d5af8d6486e5aa65181da2b1
Veronika <veronikaboeva935@gmail.com>
2023-11-09 14:32:14 +0100

    Changed annotation and deleted rows 2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.cinema.bar import CinemaBar
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int,
+def cinema_visit(customers: list[dict], hall_number: int,
                  cleaner: str, movie: str) -> None:
 
     customer_instances = [Customer(name=customer["name"],
