3182d07f1eaf245d8fe3c4592c01b8dfb9bc313e
Alex Moon <moon.ra.lex@gmail.com>
2023-11-14 19:02:53 +0100

    Done

---
+++ app/people/customer.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+
+class Customer:
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str) -> None:
+
+        print(f'{self.name} is watching \"{movie}\".')
+
+    @classmethod
+    def customer_extractor(cls, customer_list: list) -> list[Customer]:
+        return [
+            cls(
+                name=customer["name"],
+                food=customer["food"]
+            )
+            for customer in customer_list
+        ]
