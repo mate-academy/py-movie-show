115dbaa51b3b713bb647a7f612eca34251717ff2
Valeriy Semenik <valeriy.semenik@gmail.com>
2023-11-30 00:09:18 +0200

    'Solution'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,12 +5,13 @@ from app.people.cinema_staff import Cleaner
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
-    # number of the hall in cinema
 
-    def movie_session(self, movie: str, customers: list,
-                      cleaning_staff: Cleaner) -> None:
+    def movie_session(self, movie: str,
+                      customers: list,
+                      cleaning_staff: Cleaner
+                      ) -> None:
         print(f'"{movie}" started in hall number {self.number}.')
-        for cs in customers:
-            cs.watch_movie(movie)
+        for customer in customers:
+            customer.watch_movie(movie)
         print(f'"{movie}" ended.')
         cleaning_staff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -5,22 +5,20 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str,
-                 movie: str) -> None:
-    cb = CinemaBar()
-    ch = CinemaHall(hall_number)
-    cl = Cleaner(cleaner)
-    cs_ = [Customer(name=cs_p["name"], food=cs_p["food"])
-           for cs_p in customers]
-    for i in customers:
-        cs = Customer(i["name"], i["food"])
-        cb.sell_product(customer=cs, product=cs.food)
-    ch.movie_session(movie=movie, customers=cs_, cleaning_staff=cl)
-
-# Cinema bar sold Coca-cola to Bob.
-# Cinema bar sold popcorn to Alex.
-# "Madagascar" started in hall number 5.
-# Bob is watching "Madagascar".
-# Alex is watching "Madagascar".
-# "Madagascar" ended.
-# Cleaner Anna is cleaning hall number 5.
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str
+                 ) -> None:
+    c_bar = CinemaBar()
+    c_hall = CinemaHall(hall_number)
+    c_cleaner = Cleaner(cleaner)
+    c_customers = [Customer(name=customer["name"], food=customer["food"])
+                   for customer in customers]
+    for customer in customers:
+        custmr = Customer(customer["name"], customer["food"])
+        c_bar.sell_product(customer=custmr, product=custmr.food)
+    c_hall.movie_session(movie=movie,
+                         customers=c_customers,
+                         cleaning_staff=c_cleaner
+                         )
