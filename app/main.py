09a6e46d61d04dddfb185dfe290b1069dc27c4b2
Artur Komyshniuk <akomisnuk@gmail.com>
2023-12-01 17:01:58 +0200

    changed the variable name сÐcustomers_list

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaning_staff: str,
                  movie_name: str) -> None:
-    customers_list = []
+    customers_instances = []
     for customer_dict in customers:
         customer = Customer(customer_dict["name"], customer_dict["food"])
-        customers_list.append(customer)
+        customers_instances.append(customer)
         CinemaBar.sell_product(customer, customer.food)
     hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaning_staff)
-    hall.movie_session(movie_name, customers_list, cleaner)
+    hall.movie_session(movie_name, customers_instances, cleaner)
