15602520455b17cb6a9e583c5d214ceb397131b8
Alisa <alisasklrv@gmail.com>
2023-12-02 17:14:39 +0200

    solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,20 @@
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list[Customer],
+            cleaner: Cleaner
+    ) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
+        for customer_instance in customers:
+            customer_instance.watch_movie(movie_name)
+        print(f'"{movie_name}" ended.')
+        cleaner.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    customers_list = []
+    for inst in customers:
+        customer_inst = Customer(inst["name"], inst["food"])
+        customers_list.append(customer_inst)
+    cinema_hall = CinemaHall(hall_number)
+    cleaner_inst = Cleaner(cleaner)
+    for person in customers_list:
+        CinemaBar.sell_product(person, person.food)
+    cinema_hall.movie_session(movie, customers_list, cleaner_inst)
