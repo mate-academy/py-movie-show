47a25843a1dadf5c2a4f91e04a187bccbf685a98
Keryit <fillimonova@inbox.ru>
2023-12-04 15:46:10 +0100

    added[dict] to customers: list[dict]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int,
+def cinema_visit(customers: list[dict], hall_number: int,
                  cleaner: str, movie: str) -> None:
     customers_list = []
 
