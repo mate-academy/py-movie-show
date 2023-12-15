53962165d42317bc8aea26ea91432cad25009bbb
Dariia Shostak <davorobii@gmail.com>
2023-12-15 16:26:40 +0200

    fix flake8 tests

--- app/main.py
+++ app/main.py
@@ -9,12 +9,16 @@ def cinema_visit(customers: list,
                  cleaner: str,
                  movie_name: str) -> None:
     customer_instances = []
+
     for customer in customers:
         instance = Customer(customer["name"], customer["food"])
         customer_instances.append(instance)
+
     hall = CinemaHall(hall_number)
     stuff = Cleaner(cleaner)
     cinema_bar = CinemaBar()
+
     for customer in customer_instances:
         cinema_bar.sell_product(customer.food, customer)
+
     hall.movie_session(movie_name, customer_instances, stuff)
