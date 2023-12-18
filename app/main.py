5fb6df8ba5165ac5abdab681436fb81831225078
angelzagniy <angelzagniy@gmail.com>
2023-12-18 16:36:09 +0200

    fix spaces

--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     cleaning = Cleaner(cleaner)
     list_of_customers = []
-    for cust in customers:
-        new_customer = Customer(cust["name"], cust["food"])
+    for customer in customers:
+        new_customer = Customer(customer["name"], customer["food"])
         list_of_customers.append(new_customer)
         CinemaBar.sell_product(new_customer, new_customer.food)
     CinemaHall(hall_number).movie_session(movie, list_of_customers, cleaning)
