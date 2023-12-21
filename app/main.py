747184c160adc9dbaed9a364ce815827e55dfbc4
Oleksandr <binsasha97@gmail.com>
2023-12-21 14:14:22 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-# write your imports here
+from __future__ import annotations
+
+from app.cinema.bar import CinemaBar
+from app.people.customer import Customer
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+
+    cleaning = Cleaner(cleaner)
+    # ----------------------------------how many customers came to the movie
+    for person in customers:
+        if isinstance(person, Customer):
+            CinemaBar.sell_product(person, person.food)
+        else:
+            customer = Customer(person["name"], person["food"])
+            CinemaBar.sell_product(customer, customer.food)
+
+    CinemaHall(hall_number).movie_session(movie, customers, cleaning)
