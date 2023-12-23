9125ab5a53c0e6e219770147d675bc25e7e07cb1
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-23 21:21:50 +0100

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,11 @@
+class CinemaHall:
+    def __init__(self, number: int):
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers,
+                      cleaning_staff):
+        print(f""""{movie_name}" started in hall number {self.number}.""")
+        for client in customers:
+            client.watch_movie(movie_name)
+        print(f""""{movie_name}" ended.""")
+        cleaning_staff.clean_hall(self.number)
