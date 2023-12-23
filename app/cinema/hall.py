be9e4dd070ed733e368353e9256b2fcc73c50f0e
cyb1ove <cyb1ove.ua@gmail.com>
2023-12-23 15:08:17 +0200

    codestyle fixed

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -3,16 +3,16 @@ from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
-  def __init__(self, number: int) -> None:
-    self.number = number
+    def __init__(self, number: int) -> None:
+        self.number = number
 
-  def movie_session(self, movie_name: str,
+    def movie_session(self, movie_name: str,
                     customers: list[Customer], cleaner_staff: Cleaner) -> None:
-    print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
 
-    for customer in customers:
-      customer.watch_movie(movie_name)
+        for customer in customers:
+            customer.watch_movie(movie_name)
 
-    print(f"\"{movie_name}\" ended.")
+        print(f"\"{movie_name}\" ended.")
 
-    cleaner_staff.clean_hall(self.number)
+        cleaner_staff.clean_hall(self.number)
