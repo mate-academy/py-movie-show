da56061a91b63000c7640b18e53d173c70b44968
Maksym Symonovych <87976005+msymonovych@users.noreply.github.com>
2024-01-04 15:21:15 +0200

    Update app/cinema/hall.py
    Co-authored-by: Paul Savchuk <107141441+NEkropo1@users.noreply.github.com>

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,7 +12,9 @@ class CinemaHall:
             clearing_staff: Callable
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
+        
         for customer in customers:
             customer.watch_movie(movie_name)
+        
         print(f'"{movie_name}" ended.')
         clearing_staff.clean_hall(self.number)
