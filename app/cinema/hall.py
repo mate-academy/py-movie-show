21d9c72cef44afa7fc0d37ad54232b049128cfbe
PythonZem <pythonzem@gmail.com>
2024-01-12 19:32:39 +0200

    add class Customer

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,7 +8,6 @@ class CinemaHall:
     def movie_session(self, movie_name: str, customers: list[Customers], cleaning_stuff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            customer.watch_movie()
+            customer.watch_movie(movie=movie_name)
         print(f'"{movie_name}" ended')
         cleaning_stuff.clean_hall(number=self.number)
-
