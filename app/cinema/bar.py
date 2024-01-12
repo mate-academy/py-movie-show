5e089121913d81ce87a82f38ed3d4fbfd2d7551f
PythonZem <pythonzem@gmail.com>
2024-01-12 20:12:04 +0200

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 
 class CinemaBar:
     @staticmethod
-    def sell_product(self, customer: Customer, product: str) -> None:
-        print(f"Cinema bar sold {product} to {customer.name}")
+    def sell_product(customer: any, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,13 +1,16 @@
-from __future__ import annotations
-
-
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list[Customers], cleaning_stuff: Cleaner) -> None:
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: list,
+            cleaning_stuff: any
+    ) -> None:
+
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie=movie_name)
-        print(f'"{movie_name}" ended')
-        cleaning_stuff.clean_hall(number=self.number)
+        print(f'"{movie_name}" ended.')
+        cleaning_stuff.clean_hall(hall_number=self.number)
