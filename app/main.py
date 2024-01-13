fb6243dccd8f22ec1bf67bb11c61dba331eddb86
Ant <u123@ua.fm>
2024-01-13 16:20:05 +0200

    Solution - fixing quotes

--- app/main.py
+++ app/main.py
@@ -6,9 +6,8 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
-    customers_obj = []
-    for customer in customers:
-        customers_obj.append(Customer(customer["name"], customer["food"]))
+    customers_obj = [Customer(customer["name"], customer["food"])
+                     for customer in customers]
     hall = CinemaHall(hall_number)
     cbar = CinemaBar()
     cleaner_obj = Cleaner(cleaner)
