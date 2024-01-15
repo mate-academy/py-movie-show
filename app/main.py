5265a99f885bb8bc70a7b7e5923ddf88eab73e2e
Maksym Operchuk <operlast529@gmail.com>
2024-01-15 21:44:16 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,18 +4,20 @@ from app.cinema.bar import CinemaBar
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(spectators: list[dict],
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str
-                 ) -> None:
+def cinema_visit(
+        spectators: list[dict],
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
 
     persons = []
+
     for human in spectators:
         name = human["name"]
         food = human["food"]
         person = Customer(name, food)
         persons.append(person)
-
         CinemaBar.sell_product(person.food, person)
+
     CinemaHall(hall_number).movie_session(movie, persons, Cleaner(cleaner))
