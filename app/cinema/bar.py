540f5f6a946c7508d278f207bdb8030801c097d4
Nick Illich <nickillich96@gmail.com>
2024-01-15 06:21:40 +0200

    'Solution'

---
+++ app/cinema/bar.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from app.people.customer import Customer
+
+
+class CinemaBar:
+    """Describes work of cinema bar"""
+
+    @staticmethod
+    def sell_product(product: str, customer: Customer) -> None:
+        """Prints what product and to whom cinema sold"""
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+
+    customers_list = [
+        Customer(
+            name=customer["name"],
+            food=customer["food"]
+        ) for customer in customers
+    ]
+
+    hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner)
+    bar = CinemaBar()
+
+    for guest in customers_list:
+        bar.sell_product(product=guest.food, customer=guest)
+
+    hall.movie_session(movie_name=movie,
+                       customers=customers_list,
+                       cleaning_staff=cleaner)
