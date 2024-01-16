826249d8f916347dc7678ae7935866038b7851e1
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-01-16 13:15:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int,
+def cinema_visit(customers: list[dict], hall_number: int,
                  cleaner: str, movie: str) -> None:
     customer_instances = [Customer(customer["name"],
                           customer["food"]) for customer in customers]
