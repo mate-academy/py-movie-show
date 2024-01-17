bd193227e3ae53a02cb3a78f2699c02097c15383
Artem Chernyshov <achernyshovv1@gmail.com>
2024-01-17 19:15:47 +0200

    Soltion

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,12 +1,19 @@
+from app.people.cinema_staff import Cleaner
 from typing import List
+
+from app.people.customer import Customer
 
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str,
-                      customers: List, cleaning_staff: str) -> None:
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: List[Customer],
+            cleaning_staff: Cleaner
+    ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
--- app/main.py
+++ app/main.py
@@ -1,39 +1,31 @@
+from __future__ import annotations
 from typing import List
+
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: List,
-                 hall_number: int,
-                 cleaner_name: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: List[dict],
+        hall_number: int,
+        cleaner_name: str,
+        movie: str
+) -> None:
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(number=hall_number)
     cleaner = Cleaner(name=cleaner_name)
 
-    for customer_info in customers:
-        customer = Customer(name=customer_info["name"],
-                            food=customer_info["food"])
+    customer_instances = [
+        Customer(name=customer["name"], food=customer["food"])
+        for customer in customers
+    ]
+
+    for customer in customer_instances:
         cinema_bar.sell_product(customer=customer, product=customer.food)
-    cinema_hall.movie_session(
-        movie_name=movie,
-        customers=[Customer(name=customer["name"],
-                            food=customer["food"]) for customer in customers],
-        cleaning_staff=cleaner
-    )
 
-
-customers = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-
-cinema_visit(customers=customers,
-             hall_number=hall_number,
-             cleaner_name=cleaner_name,
-             movie=movie)
+    cinema_hall.movie_session(movie_name=movie,
+                              customers=customer_instances,
+                              cleaning_staff=cleaner)
