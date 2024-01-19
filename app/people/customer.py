db7cfa67df2a2d10a1950aaba78324b755ca4493
Sofia Zholud <szholuds@gmail.com>
2024-01-19 14:55:25 +0200

    'Solution'

--- app/people/customer.py
+++ app/people/customer.py
@@ -1,8 +1,7 @@
-# app/people/customer.py
 class Customer:
     def __init__(self, name: str, food: str) -> None:
         self.name = name
         self.food = food
 
     def watch_movie(self, movie: str) -> None:
-        print(f"{self.name} is watching \"{movie}\".")
+        print(f'{self.name} is watching "{movie}".')
