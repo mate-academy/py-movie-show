1c9ce505eca1a4e70f6c5809102a754b1a07fa0b
Grigoriy Gesiod <g.gesiod@gmail.com>
2024-01-23 20:33:34 +0200

    remove comments and little refactoring/renamed variables

--- app/main.py
+++ app/main.py
@@ -11,21 +11,21 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-    # Create instances
     customers_instances = [
-        Customer(name=cust["name"],
-                 food=cust["food"]
-                 ) for cust in customers]
-    cb = CinemaBar()
-    ch = CinemaHall(number=hall_number)
+        Customer(
+            name=customer["name"],
+            food=customer["food"]
+        )
+        for customer in customers]
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(number=hall_number)
 
     cleaner = Cleaner(name=cleaner)
 
-    # Sell food to customers
     for customer in customers_instances:
-        cb.sell_product(product=customer.food, customer=customer)
+        cinema_bar.sell_product(product=customer.food, customer=customer)
 
-    # Start movie session
-    ch.movie_session(movie_name=movie,
-                     customers=customers_instances,
-                     cleaning_staff=cleaner)
+    cinema_hall.movie_session(
+        movie_name=movie,
+        customers=customers_instances,
+        cleaning_staff=cleaner)
