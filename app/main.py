d7af6f6671da25abbd760a9c2429db0797ab1991
volry <v.v.ryzhuk@gmail.com>
2024-01-25 21:43:56 +0200

    code is working

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
-    cleaner = Cleaner(cleaner)
-    
-    customer_object = []
+    cleaner_instance = Cleaner(cleaner)
+
+    customer_objects = []
     for customer_info in customers:
         customer = Customer(customer_info['name'], customer_info['food'])
-        cinema_bar.sell_product(customer.food, customer)
+        cinema_bar.sell_product(product=customer.food, customer=customer)
         customer_objects.append(customer)
 
-    cinema_hall.movie_session(movie_name, customer_objects, cleaner)
-
+    cinema_hall.movie_session(movie, customer_objects, cleaner_instance)
