2625ce07f8bf11179e5f7bca2dc778e9ac0bf4d8
volry <v.v.ryzhuk@gmail.com>
2024-01-25 21:20:18 +0200

    ekran string

--- app/people/customer.py
+++ app/people/customer.py
@@ -3,5 +3,5 @@ class Customer:
   def __init__(self, name, food) -> None:
     self.name = name
   
-  def watch_movie(movie: str):
-    print(f"# Bob is watching "Madagascar".")
+  def watch_movie(self, movie: str):
+    print(f"{self.name} is watching \"{movie}\".") 
