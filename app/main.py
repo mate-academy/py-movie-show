cf51a5e06efe895a2534d831475e088d149c285c
voronvasia <voronvasia@gmail.com>
2024-01-27 17:21:23 +0200

    fixed code

--- app/main.py
+++ app/main.py
@@ -4,13 +4,11 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(
+    customers: list, hall_number: int, cleaner: str, movie: str
+) -> None:
     list_customers = [
-        Customer(
-            customer["name"],
-            customer["food"]
-        )
-        for customer in customers
+        Customer(customer["name"], customer["food"]) for customer in customers
     ]
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
