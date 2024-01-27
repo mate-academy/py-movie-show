6db6dfe9c07cf314cf66cc2e8edeeafb9181a691
volry <v.v.ryzhuk@gmail.com>
2024-01-27 12:26:13 +0200

    fix annotation in List[Customer]

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,4 @@
+from typing import List
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
@@ -7,7 +8,7 @@ class CinemaHall:
         self.number = number
 
     def movie_session(self, movie_name: str,
-                      customers: Customer,
+                      customers: List[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
