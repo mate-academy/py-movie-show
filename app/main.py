038d43001751eb6f362a6a46f25a64141a9a375a
Vitalii <warcied1@gmail.com>
2024-01-28 12:49:31 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -4,16 +4,20 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     cb = CinemaBar()
     ch = CinemaHall(hall_number)
     curr_cleaner = Cleaner(cleaner)
 
     customers_obj_list = []
     for customer_data in customers:
-        curr_customer = Customer(customer_data.get("name"), customer_data.get("food"))
+        curr_customer = Customer(customer_data.get("name"),
+                                 customer_data.get("food"))
         customers_obj_list.append(curr_customer)
         if curr_customer.food:
             cb.sell_product(curr_customer.food, curr_customer)
 
-    ch.movie_session(movie,customers_obj_list, curr_cleaner)
+    ch.movie_session(movie, customers_obj_list, curr_cleaner)
