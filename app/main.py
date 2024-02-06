17c0523b1ad704cf92a82a96a78bf58520affce0
Mikola Rozhansky <bymikola@gmail.com>
2024-02-06 23:09:35 +0200

    cinemabar

--- app/main.py
+++ app/main.py
@@ -5,3 +5,22 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
 
+    inst_customers: list[Customer]
+    inst_cinemahall: CinemaHall
+    inst_cinemabar: list[CinemaBar]
+    inst_clener: Cleaner
+
+    inst_customers  = []
+
+    for customer in customers:
+        instCustomers.append(Customer(name=customer["name"], food=customer["food"]))
+
+
+
+
+
+
+
+
+
+
