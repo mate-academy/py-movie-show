edb0974cf7b664049a9b542bb2ddb3bd07fc7fda
Mikola Rozhansky <bymikola@gmail.com>
2024-02-06 23:27:41 +0200

    cinemabar

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,7 +1,7 @@
 class CinemaBar:
     @staticmethod
     def sell_product(customer, product: str) -> None:
-        print(f"Cinema bar sold {product} to {customer.name}")
+        print(f"Cinema bar sold {product} to {customer.name}.")
 
 
 
--- app/main.py
+++ app/main.py
@@ -3,12 +3,9 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
+
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-
     inst_customers: list[Customer]
-    # inst_cinemabar: list[CinemaBar]
-    inst_clener: Cleaner
-
     inst_customers = []
 
     for customer in customers:
@@ -18,6 +15,8 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
         CinemaBar.sell_product(customer=customer, product=customer.food)
 
     CinemaHall(number=hall_number).movie_session(movie_name=movie, customers=inst_customers, cleaning_staff=Cleaner(cleaner))
+
+
 
 
 
