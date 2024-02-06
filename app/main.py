610c6f0d3ba6536e0014bf62da1bcd5501dcb90f
Mikola Rozhansky <bymikola@gmail.com>
2024-02-06 23:46:04 +0200

    cinemabar

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     inst_customers: list[Customer]
     inst_customers = []
 
