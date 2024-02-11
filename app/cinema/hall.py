3b22e376c14d0508f1e562dfdb6765f66f3921d5
Andrei Butko <andreybutkomerefa@gmail.com>
2024-02-11 22:49:49 +0200

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,26 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self, movie_name: str,
+                      customers: list,
+                      cleaning_staff: Cleaner) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
+
+        if type(customers[0]) is dict:
+            customers_list = []
+            for customer in customers:
+                customer = Customer(name=customer["name"],
+                                    food=customer["food"])
+                customers_list.append(customer)
+        else:
+            customers_list = customers
+
+        for customer in customers_list:
+            customer.watch_movie(movie=movie_name)
+        print(f'"{movie_name}" ended.')  # ended
+        cleaning_staff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(movie: str,
+                 customers: Customer,
+                 hall_number: int, cleaner: str) -> None:
+
+    if not isinstance(customers, list):
+        movie, customers, hall_number, cleaner =\
+            cleaner, movie, customers, hall_number
+
+    cinema_bar = CinemaBar()
+    customers_list = []
+    cleaner = Cleaner(name=cleaner)
+    hall = CinemaHall(number=hall_number)
+
+    for customers_info in customers:
+        customer = Customer(name=customers_info["name"],
+                            food=customers_info["food"])
+        cinema_bar.sell_product(customer=customer,
+                                product=customer.food)
+        customers_list.append(customers_info)
+
+    hall.movie_session(
+        movie_name=movie,
+        customers=customers_list,
+        cleaning_staff=cleaner
+    )
