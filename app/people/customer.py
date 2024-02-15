c6ddc694fd1e56cf807d2d3c6c725807db596e32
StanislavBlashchuk <stanislav.blashchuk@cinarra.com>
2024-02-15 17:51:07 +0700

    work in progress

---
+++ app/people/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+
+    def __iter__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str) -> None:
+        print(f"{self.name} is watching {movie}.")
