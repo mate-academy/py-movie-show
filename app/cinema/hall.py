060bf2a52c45ebd7aa0dd52652ecab6053e73f40
Maksym Hekalo <maximgekalo@gmail.com>
2024-02-17 20:32:22 +0200

    fix - Change outer quotes to avoid escaping

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,8 +8,8 @@ class CinemaHall:
                       customers: list[object],
                       cleaning_staff: object,
                       ) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer_obj in customers:
             customer_obj.watch_movie(movie_name)
-        print(f"\"{movie_name}\" ended.")
+        print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
