676b7d061f1202306d3d758b340e4960ab3bb0fd
Viacheslav Pavlenko <vyacheslav.p2024@gmail.com>
2024-02-23 10:58:40 +0200

    Order imports alphabetically

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
-
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list,
