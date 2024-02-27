ff3e22cd9ab7b68ff3ab24cea232f95811a89fe7
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-02-27 12:23:12 +0200

    improved the code style and readability. changes to the .md file were accidentally made

--- app/main.py
+++ app/main.py
@@ -4,16 +4,20 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str) -> None:
-    list_of_customer = []
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> list[Customer]:
+    list_of_customers = []
     for customer in customers:
         new_customer = Customer(customer["name"], customer["food"])
-        list_of_customer.append(new_customer)
+        list_of_customers.append(new_customer)
 
     cinema_hall = CinemaHall(hall_number)
     cinema_bar = CinemaBar()
     cinema_cleaner = Cleaner(cleaner)
-    for person in list_of_customer:
+    for person in list_of_customers:
         cinema_bar.sell_product(person.food, person)
-    cinema_hall.movie_session(movie, list_of_customer, cinema_cleaner)
+    cinema_hall.movie_session(movie, list_of_customers, cinema_cleaner)
