19a0da2c79dd8ebae8f14bc583675ab7e4e23744
skeletronys <mykytcey@gmail.com>
2024-02-29 19:44:14 +0100

    solution

--- app/people/customer.py
+++ app/people/customer.py
@@ -4,7 +4,7 @@ class Customer():
         self.food = food
 
     def watch_movie(self, movie: str) -> None:
-        print(f"{self.name} is watching \"{movie}\".")
+        print(f'{self.name} is watching \"{movie}\".')
 
 
 class Cleaner():
