ef82f0243630e4de43e351f83a01293e2176c5e9
Lumideus <d.bilan2004@gmail.com>
2024-03-01 00:47:12 +0200

    Implement cinema

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-# write your imports here
+from __future__ import annotations
+
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict], hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    customers_insts = []
+    hall_instance = CinemaHall(hall_number)
+    cleaner_instance = Cleaner(cleaner)
+    for customer in customers:
+        customers_insts.append(Customer(customer["name"], customer["food"]))
+    for customer in customers_insts:
+        CinemaBar.sell_product(customer, customer.food)
+    hall_instance.movie_session(movie, customers_insts, cleaner_instance)
