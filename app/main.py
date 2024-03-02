18de52c1ae0d5ac766ac5ab519950833ea152962
Vitalii Haponiuk <doninpotato@gmail.com>
2024-03-02 15:51:35 +0200

    'Corrected2'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def cinema_visit(
     movie: str,
 ) -> None:
     customers = [Customer(customer["name"], customer["food"])
-                       for customer in customers]
+                 for customer in customers]
     for element in customers:
         CinemaBar.sell_product(customer=element, product=element.food)
     hall = CinemaHall(hall_number)
