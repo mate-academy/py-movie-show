67162158bfdfcb307dc360b7592c1b0eef389493
Olha <olgazinevych@gmail.com>
2024-03-05 19:21:59 +0200

    Fixed flake8 for main

--- app/main.py
+++ app/main.py
@@ -4,11 +4,9 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     for i, cstmr in enumerate(customers):
         customers[i] = Customer(cstmr["name"], cstmr["food"])
         CinemaBar.sell_product(customers[i].food, customers[i])
     CinemaHall(hall_number).movie_session(movie, customers, Cleaner(cleaner))
-
-
-
