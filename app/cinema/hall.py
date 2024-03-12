7158da3bd83e7cfb7c6f0cfac79d46161a990d42
Shershen Alexandra <hornet240204@gmail.com>
2024-03-12 23:14:26 +0200

    Create package cinema and modules: bar.py and hall.py. Implement classes CinemaBar and CinemaHall

---
+++ app/cinema/hall.py
@@ -0,0 +1,12 @@
+class CinemaHall:
+    def __init__(self, number: int):
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers: list[Customer], cleaning_staff: Cleaner):
+        print(f"Movie session for {movie_name} started in Hall {self.number}.")
+
+        for customer in customers:
+            customer.watch_movie(movie_name)
+
+        print(f"Movie session for {movie_name} ended in Hall {self.number}.")
+        cleaning_staff.clean_hall()
