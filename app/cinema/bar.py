520a8be21dfc3cbc9284b567e7ed4fde5867f311
Shershen Alexandra <hornet240204@gmail.com>
2024-03-12 23:43:10 +0200

    Add imports and implement class Cleaner

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,5 +1,7 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
     @staticmethod
     def sell_product(product, customer: Customer):
         print(f"Cinema bar sold {product} to {customer.name}.")
-
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,6 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
 class CinemaHall:
     def __init__(self, number: int):
         self.number = number
