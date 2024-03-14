7b678e052621acc6c6226321124a58d015ec6667
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-03-14 20:52:18 +0200

    Correct solution without escaping

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -15,5 +15,5 @@ class CinemaHall:
         print(f""""{movie_name}" started in hall number {self.number}.""")
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f"""""{movie_name}" ended.""")
+        print(f""""{movie_name}" ended.""")
         cleaning_staff.clean_hall(self.number)
