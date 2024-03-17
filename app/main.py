d8da33b88e52dc07d2aba45408409487a8e9489d
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-03-16 22:30:52 -0700

    exported PYTHONPATH

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from cinema.bar import CinemaBar
-from cinema.hall import CinemaHall
-from people.customer import Customer
-from people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int,
