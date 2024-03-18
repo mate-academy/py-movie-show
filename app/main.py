a2e6ee25ffb7ff79094d22c28f594cc8d9bb4fbc
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-03-18 06:15:33 -0700

    import classes directly

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from app.cinema.bar import CinemaBar
-from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int,
