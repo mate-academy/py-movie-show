29226ce2056a77f1aeeffbd1bb3af3081b67d9f7
NioviIki <charmniovi@tutanota.com>
2024-03-20 16:17:40 +0200

    added cinema_visit in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list, hall_number: int, cleaner: str, movie: str
+) -> None:
+    customers = [Customer(**customer) for customer in customers]
+    list(map(lambda x: CinemaBar().sell_product(x, x.food), customers))
+    CinemaHall(hall_number).movie_session(movie, customers, Cleaner(cleaner))
