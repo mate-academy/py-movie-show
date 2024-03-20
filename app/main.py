89800641cb78d146b36f22c29e1ebf0ab149d3c4
Oleh <musiychuk09@gmail.com>
2024-03-20 21:35:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
-from cinema.bar import CinemaBar
-from cinema.hall import CinemaHall
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    for customer in customers:
-        CinemaBar.sell_product(customer["name"], customer["food"])
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    list_of_customers = []
+    for element in customers:
+        customer = Customer(element["name"], element["food"])
+        CinemaBar.sell_product(customer, customer.food)
+        list_of_customers.append(customer)
 
-    CinemaHall(hall_number).movie_session(movie, customers, cleaner)
-
-
-
-customers = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
-# Cinema bar sold Coca-cola to Bob.
-# Cinema bar sold popcorn to Alex.
-# "Madagascar" started in hall number 5.
-# Bob is watching "Madagascar".
-# Alex is watching "Madagascar".
-# "Madagascar" ended.
-# Cleaner Anna is cleaning hall number 5.
+    CinemaHall(hall_number).movie_session(
+        movie,
+        list_of_customers,
+        Cleaner(cleaner)
+    )
