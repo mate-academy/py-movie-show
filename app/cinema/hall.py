5354c88019bdd1dc5a20b636afe7923ea6e32779
NioviIki <charmniovi@tutanota.com>
2024-03-20 16:23:43 +0200

    reworked CinemaHall

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,7 +12,6 @@ class CinemaHall:
             cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
-        for customer in customers:
-            customer.watch_movie(movie_name)
+        list(map(lambda x: x.watch_movie(movie_name), customers))
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
