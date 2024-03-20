40f603237c0d8bf86dd042509e95a85f811dbb6a
Oleh <musiychuk09@gmail.com>
2024-03-20 17:01:14 +0200

    Import does not work

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-# write your imports here
+from cinema.bar import CinemaBar
+from cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    for customer in customers:
+        CinemaBar.sell_product(customer["name"], customer["food"])
+
+    CinemaHall(hall_number).movie_session(movie, customers, cleaner)
+
+
+
+customers = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
+hall_number = 5
+cleaner_name = "Anna"
+movie = "Madagascar"
+cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
+# Cinema bar sold Coca-cola to Bob.
+# Cinema bar sold popcorn to Alex.
+# "Madagascar" started in hall number 5.
+# Bob is watching "Madagascar".
+# Alex is watching "Madagascar".
+# "Madagascar" ended.
+# Cleaner Anna is cleaning hall number 5.
