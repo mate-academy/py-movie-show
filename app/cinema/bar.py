422aeaa0b442ab8e7e9ab8477c2f4f6fb0c2c5e3
semermaks <semermaks@mail.com>
2024-03-21 13:50:43 +0200

    flake8 errors fix

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,6 @@
 from app.people.customer import Customer
+
+
 class CinemaBar:
     @staticmethod
     def sell_product(product: str, customer: Customer) -> None:
--- app/main.py
+++ app/main.py
@@ -4,13 +4,16 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     hall = CinemaHall(hall_number)
     customers_list = []
     cleaner = Cleaner(cleaner)
     for customer in customers:
-        cust = Customer(customer['name'], customer['food'])
+        cust = Customer(customer["name"], customer["food"])
         customers_list.append(cust)
-        CinemaBar.sell_product(customer['food'], cust)
+        CinemaBar.sell_product(customer["food"], cust)
 
     hall.movie_session(movie, customers_list, cleaner)
