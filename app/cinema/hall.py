342c7364ef5b89dfd56be2c65d56f3220e0ec5e0
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-21 10:05:40 +0200

    Fixed call method in hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,6 +12,6 @@ class CinemaHall:
                       cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            Customer.watch_movie(customer, movie_name)
+            customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         Cleaner.clean_hall(cleaning_staff, self.number)
