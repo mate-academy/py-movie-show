a91049e6ef5f5e43830476615bc0b114e3497119
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-22 08:09:19 +0100

    made one iteration

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list[dict], hall_number: int,
-                 cleaner: str, movie: str) -> None:
-    customer_arr = list(map(lambda customer: Customer(
-        customer["name"], customer["food"]), customers))
-    [CinemaBar.sell_product(customer,
-                            customer.food) for customer in customer_arr]
+                 cleaner: str,
+                 movie: str) -> None:
+    customer_arr = []
+    for customer in customers:
+        customer_obj = Customer(customer["name"], customer["food"])
+        customer_arr.append(customer_obj)
+        CinemaBar().sell_product(customer_obj, customer_obj.food)
     CinemaHall.movie_session(CinemaHall(hall_number),
                              movie, customer_arr, Cleaner(cleaner))
