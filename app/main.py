51a8421265d018ec9920582bf4fe3bacdc8d7f4c
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-22 07:28:36 +0100

    Added specific type to annotation and changed list comprehension to one iteration

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
-    customer_arr = [Customer(customer["name"],
-                             customer["food"]) for customer in customers]
+def cinema_visit(customers: list[dict], hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    customer_arr = list(map(lambda customer: Customer(
+        customer["name"], customer["food"]), customers))
     [CinemaBar.sell_product(customer,
                             customer.food) for customer in customer_arr]
     CinemaHall.movie_session(CinemaHall(hall_number),
