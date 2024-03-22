53f36ea4dce1c590b79c0d93eca35603b18ee537
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-22 11:59:48 +0100

    called methods from class instances

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list[dict], hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
     customer_arr = []
+    cinema_hall = CinemaHall(hall_number)
     for customer in customers:
         customer_obj = Customer(customer["name"], customer["food"])
         customer_arr.append(customer_obj)
         CinemaBar().sell_product(customer_obj, customer_obj.food)
-    CinemaHall.movie_session(CinemaHall(hall_number),
-                             movie, customer_arr, Cleaner(cleaner))
+    cinema_hall.movie_session(movie, customer_arr, Cleaner(cleaner))
