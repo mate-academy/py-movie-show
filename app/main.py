57b36deb2269b249e1413a1911045446f78610cf
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-03-22 16:54:11 +0200

    change for one iteration to create customer and sell him a product

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ def cinema_visit(
         movie: str
 ) -> None:
     cleaner_instance = Cleaner(cleaner)
-    customers_list = [
-        Customer(customer["name"], customer["food"]) for customer in customers
-    ]
     cinema_hall = CinemaHall(hall_number)
-    for customer in customers_list:
+    customer_objects = []
+    for data in customers:
+        customer = Customer(data["name"], data["food"])
         CinemaBar.sell_product(customer.food, customer)
+        customer_objects.append(customer)
 
-    cinema_hall.movie_session(movie, customers_list, cleaner_instance)
+    cinema_hall.movie_session(movie, customer_objects, cleaner_instance)
