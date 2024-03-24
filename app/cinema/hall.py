e597a07b4ed41b3af3f55b43c18ec2e82f38f230
Sofiia <sonagm9@gmail.com>
2024-03-24 19:11:04 +0200

    'Solution2'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,8 +10,8 @@ class CinemaHall:
     def movie_session(self, movie: str,
                       customers: List[Customer],
                       cleaning_staff: Cleaner) -> None:
-        print(f'"{movie}\" started in hall number {self.number}.')
+        print(f'"{movie}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie)
-        print(f'"{movie}\" ended.')
+        print(f'"{movie}" ended.')
         cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -5,14 +5,15 @@ from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int,
-                 cleaner: Cleaner | str, movie: str) -> None:
+                 cleaner: str, movie: str) -> None:
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
 
-    for customer_data in customers:
-        customer = Customer(name=customer_data["name"],
-                            food=customer_data["food"])
+    customers_list = [Customer(name=c["name"], food=c["food"])
+                      for c in customers]
+
+    for customer in customers_list:
         cinema_bar.sell_product(customer, customer.food)
-    cinema_hall.movie_session(movie,
-                              [Customer(**c) for c in customers], cleaner)
+
+    cinema_hall.movie_session(movie, customers_list, cleaner)
