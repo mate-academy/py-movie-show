f161f521808794ae7589130e96fd72f899768c9d
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-03-25 23:05:36 +0200

    changed quotes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,8 +12,8 @@ class CinemaHall:
             self, movie_name: str,
             customers: list[Customer], cleaning_staff: Cleaner
     ) -> None:
-        print(f"'{movie_name}' started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f"'{movie_name}' ended.")
+        print(f'"{movie_name}" ended.')
         Cleaner.clean_hall(cleaning_staff, self.number)
