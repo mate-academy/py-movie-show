c5300fb9cd580c539d4b3071a3eb554e6153fa96
dvkim28 <d.villarionovich@gmail.com>
2024-04-02 22:52:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 # write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    custom_list = [Customer(customer["name"], customer["food"])
+                   for customer in customers]
+    hall_number = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner)
+
+    for customer in custom_list:
+        CinemaBar.sell_product(customer.food, customer)
+    hall_number.movie_session(movie_name=movie,
+                              customers=custom_list,
+                              cleaning_staff=cleaner)
--- tests/test_bar.py
+++ tests/test_bar.py
@@ -6,7 +6,6 @@ from contextlib import redirect_stdout
 from app.cinema.bar import CinemaBar
 from app.people.customer import Customer
 
-
 def test_cinema_bar_sell_product():
     name = "Alice"
     food = "Sprite"
