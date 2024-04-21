5289d872eec396a649e711098794c70566e1e1ea
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-04-21 17:32:08 +0300

    Fix solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,8 +9,8 @@ class CinemaHall:
     def movie_session(self, movie_name: str,
                       customers: list[customer.Customer],
                       cleaning_staff: cinema_staff.Cleaner) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'\"{movie_name}\" started in hall number {self.number}.')
         for person in customers:
-            print(f"{person.name} is watching \"{movie_name}\".")
-        print(f"\"{movie_name}\" ended.")
+            print(f'{person.name} is watching \"{movie_name}\".')
+        print(f'\"{movie_name}\" ended.')
         cleaning_staff.clean_hall(self.number)
