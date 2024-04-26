2efc128f7c203b48ecf6be6c4b04fd0ede5f79d0
Triukhan <tryukhan.d@gmail.com>
2024-04-26 21:28:07 +0300

    fix comments

--- app/main.py
+++ app/main.py
@@ -13,10 +13,9 @@ def cinema_visit(
     customer_list = [Customer(customer["name"], customer["food"])
                      for customer in customers]
     cinema_hall = CinemaHall(hall_number)
-    cinema_bar = CinemaBar()
     cleaner = Cleaner(cleaner)
 
     for customer in customer_list:
-        cinema_bar.sell_product(customer.food, customer)
+        CinemaBar.sell_product(customer.food, customer)
 
     cinema_hall.movie_session(movie, customer_list, cleaner)
