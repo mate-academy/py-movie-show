0e12aacb6632850150bd46af872bae5add0b456b
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-05-08 15:14:52 +0200

    Fix logic

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,7 +1,18 @@
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
-from typing import List
+from typing import List, Union
+
+
+def convert_to_customers(data: List[Union[Customer, dict]]) -> List[Customer]:
+    if isinstance(data[0], dict):
+        customers_list = []
+        for customer_data in data:
+            customer = Customer(name=customer_data["name"]
+                                , food=customer_data["food"])
+            customers_list.append(customer)
+        return customers_list
+    return data
 
 
 class CinemaHall:
@@ -17,14 +28,7 @@ class CinemaHall:
 
         print(f'"{movie_name}" started in hall number {self.number}.')
 
-        if type(customers[0]) is dict:
-            customers_list = []
-            for customer_data in customers:
-                customer_data = Customer(name=customer_data["name"]
-                                         , food=customer_data["food"])
-                customers_list.append(customer_data)
-        else:
-            customers_list = customers
+        customers_list = convert_to_customers(customers)
 
         for customer_data in customers_list:
             customer_data.watch_movie(movie=movie_name)
