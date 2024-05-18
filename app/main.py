77cd6f929c4867c7178334f3e8e4b9df76e2d571
Alex93Troy <troianovskiy93alex@gmail.com>
2024-05-18 19:15:46 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from app.people.cinema_staff import Cleaner
-from app.people.customer import Customer
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    list_customers = [Customer(person["name"], person["food"])
+def cinema_visit(
+        customers: list, hall_number: int,
+        cleaner: str, movie: str) -> None:
+        list_customers = [Customer(person["name"], person["food"])
                       for person in customers
-                      ]
-    bar = CinemaBar()
-    hall_number = CinemaHall(hall_number)
-    cleaner = Cleaner(cleaner)
-    for customer in list_customers:
-        bar.sell_product(customer,customer.food)
-    hall_number.movie_session(movie, list_customers, cleaner)
+        ]
+        bar_action = CinemaBar()
+        hall_number = CinemaHall(hall_number)
+        cleaner = Cleaner(cleaner)
+        for customer in list_customers:
+            bar_action.sell_product(customer, customer.food)
+        hall_number.movie_session(movie, list_customers, cleaner)
