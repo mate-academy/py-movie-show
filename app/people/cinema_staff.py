8718e569adcfe65c2fd8ca2adf9b0cbb9b623695
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-05-21 20:06:25 +0300

    py-cinema-visit v3

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -2,9 +2,6 @@ class Cleaner:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def __repr__(self) -> str:
-        return self.name
-
     def __str__(self) -> str:
         return self.name
 
