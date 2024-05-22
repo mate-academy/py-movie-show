cae808af57ccc866078331ca381d8d12c43e70d9
AndrewwSS <clash2clans1one@gmail.com>
2024-05-22 19:42:46 +0300

    fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
+from __future__ import annotations
 import datetime
 
 
 class Person:
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
 
     @staticmethod
-    def from_birth_year(name: str, birth_year: int):
+    def from_birth_year(name: str, birth_year: int) -> Person:
         return Person(name, datetime.date.today().year - birth_year)
 
-    def display(self):
+    def display(self) -> None:
         print(f"{self.name} (age: {self.age}). "
               f"Is adult: {Person.is_adult(self.age)}")
 
     @staticmethod
-    def is_adult(age):
+    def is_adult(age: int) -> bool:
         return age >= 18
