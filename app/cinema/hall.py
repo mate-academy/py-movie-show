c2ec6ada3a8f49699eb512134928c5f6174fd159
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-05-23 18:50:48 +0300

    resolve flake8 warnings

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class CinemaHall:
 
     def __init__(self, number: int) -> None:
@@ -7,11 +10,10 @@ class CinemaHall:
             self,
             movie_name: str,
             customers: list,
-            cleaning_staff
+            cleaning_staff: Any
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
-
--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+        customers: list, hall_number: int, cleaner: str, movie: str
+) -> None:
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
@@ -16,4 +18,6 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) ->
     for customer in customers_instance:
         cinema_bar.sell_product(customer.food, customer)
 
-    cinema_hall.movie_session(movie_name=movie, customers=customers_instance, cleaning_staff=cleaner)
+    cinema_hall.movie_session(
+        movie_name=movie, customers=customers_instance, cleaning_staff=cleaner
+    )
