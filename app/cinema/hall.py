670613c7fe7ca9994ab2f18bfc4340181dafe02d
unlie9 <unlie.devops9@gmail.com>
2024-05-25 14:03:54 +0300

    sol2

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,12 +1,13 @@
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str,
-                      customers: list, cleaning_staff: Cleaner) -> None:
+    def movie_session(self, movie_name: str, customers: list[Customer],
+                      cleaning_staff: Cleaner) -> None:
 
         print(f'"{movie_name}" started in hall number {self.number}.')
 
--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from app.people.customer import Customer
-
 from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
@@ -8,15 +7,14 @@ from app.cinema.hall import CinemaHall
 def cinema_visit(customers: list, hall_number: int,
                  cleaning_staff: str, movie_name: str) -> None:
 
-    customers = [Customer(name=customer["name"], food=customer["food"]
-                          )for customer in customers]
-
     cinema_hall = CinemaHall(number=hall_number)
     cinema_bar = CinemaBar()
     cleaner = Cleaner(name=cleaning_staff)
 
-    for customer in customers:
+    customers_list = []
+    for customer_info in customers:
+        customer = Customer(name=customer_info["name"], food=customer_info["food"])
         cinema_bar.sell_product(product=customer.food, customer=customer)
+        customers_list.append(customer)
 
-    cinema_hall.movie_session(movie_name=movie_name, customers=customers,
-                              cleaning_staff=cleaner)
+    cinema_hall.movie_session(movie_name=movie_name, customers=customers_list, cleaning_staff=cleaner)
