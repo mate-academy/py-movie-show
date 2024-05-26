d6d0a4f1629df6a268b786c43f04c04e1a8eae13
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-05-26 20:34:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+# from __future__ import annotations
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaning_staff: str,
+                 movie_name: str) -> None:
+
+    cinema_bar = CinemaBar()
+
+    customers_list = []
+
+    # Ітеруємось по dict_customers. Створюємо екземпляри Customer.
+    # продаємо їжу кожному відвідувачу
+    # Створюємо список та записуємо екземпляри Customer в нього
+    for customer_dict in customers:
+        customer = Customer(name=customer_dict["name"],
+                            food=customer_dict["food"])
+
+        cinema_bar.sell_product(customer=customer, product=customer.food)
+        customers_list.append(customer)
+
+    cleaner = Cleaner(name=cleaning_staff)
+
+    cinema_hall = CinemaHall(number=hall_number)
+
+    # Проведення сеансу фільму в кінозалі
+    cinema_hall.movie_session(movie_name=movie_name,
+                              customers=customers_list,
+                              cleaning_staff=cleaner)
