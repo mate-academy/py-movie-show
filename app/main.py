d47bb18de868d82ebaa0a08819a3a260283ecbe9
Mykola <jacktyler2391@gmail.com>
2024-06-06 20:23:20 +0300

    fix code style

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
+def cinema_visit(customers: list[dict],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
