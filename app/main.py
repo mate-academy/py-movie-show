e187607f133fc63ed737c94b8f73daedc1637c3e
Ivan <ivan.seeker6@gmail.com>
2024-06-08 10:27:46 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -6,17 +6,17 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
-    customer_instances = [Customer(name=customer["name"],
-                                   food=customer["food"])
-                          for customer in customers]
-
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(number=hall_number)
     cleaner_instance = Cleaner(name=cleaner)
 
-    for customer in customer_instances:
-        cinema_bar.sell_product(product=customer.food,
-                                customer=customer)
+    customer_instances = []
+    for customer in customers:
+        customer_instance = Customer(name=customer["name"],
+                                     food=customer["food"])
+        cinema_bar.sell_product(product=customer_instance.food,
+                                customer=customer_instance)
+        customer_instances.append(customer_instance)
 
     cinema_hall.movie_session(movie_name=movie,
                               customers=customer_instances,
