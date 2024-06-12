30a68a8b729596126c3328647e450a70e3b89833
Maksym Rozdoba <rozdobamax@gmail.com>
2024-06-12 09:57:20 +0300

    Add improvements

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
-from app.people.customer import Customer
+
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list[Customer],
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'\"{movie_name}\" started in hall number {self.number}.')
         for customer in customers:
