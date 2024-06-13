cbae36a774702450c9206555cd4e19e479fc03b0
Alex <54539kalashnikov@gmail.com>
2024-06-13 14:13:21 +0300

    Add anotation

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
+def cinema_visit(customers: list[dict],
                  hall_number: int,
                  cleaner: str, movie_name: str
                  ) -> None:
