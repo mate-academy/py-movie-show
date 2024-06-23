fe9f668191b4de8233d7daa7f38022e43810cafe
Daria Ganchukova <umi194@gmail.com>
2024-06-23 19:53:40 +0200

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,29 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list[Customer],
+                      cleaning_staff: Cleaner) -> None:
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie_name)
+        print(f"\"{movie_name}\" ended.")
+        cleaning_staff.clean_hall(self.number)
+
+
+if __name__ == "__main__":
+    ch = CinemaHall(5)
+    customers = [
+        Customer("Bob", "Coca-Cola"),
+        Customer("Alex", "popcorn")
+    ]
+    cleaner = Cleaner("Anna")
+    ch.movie_session(movie_name="Madagascar",
+                     customers=customers,
+                     cleaning_staff=cleaner)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,45 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    cb = CinemaBar()
+    ch = CinemaHall(hall_number)
+
+    customers_as_classes = [
+        Customer(customer["name"], customer["food"])
+        for customer in customers
+    ]
+    cleaner_as_class = Cleaner(cleaner)
+
+    for customer in customers_as_classes:
+        cb.sell_product(customer=customer, product=customer.food)
+
+    ch.movie_session(movie_name=movie,
+                     customers=customers_as_classes,
+                     cleaning_staff=cleaner_as_class)
+
+
+customers = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
+hall_number = 5
+cleaner_name = "Anna"
+movie = "Madagascar"
+cinema_visit(customers=customers,
+             hall_number=5,
+             cleaner="Anna",
+             movie="Madagascar")
+# Cinema bar sold Coca-cola to Bob.
+# Cinema bar sold popcorn to Alex.
+# "Madagascar" started in hall number 5.
+# Bob is watching "Madagascar".
+# Alex is watching "Madagascar".
+# "Madagascar" ended.
+# Cleaner Anna is cleaning hall number 5.
