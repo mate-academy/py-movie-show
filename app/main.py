0534fe1680cdd89db6b3160ce9d80ec55dd67909
Maksim <golevmaksim52@gmail.com>
2024-07-02 21:55:25 +0300

    The function ensures the correct sequence of actions in the cinema from selling food to cleaning the hall after the end of the show

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str) -> None:
+    customer_objects = \
+        [Customer(c["name"], c["food"]) for c in customers]
+
+    cinema_bar = CinemaBar()
+    cleaner = Cleaner(cleaner)
+
+    for customer in customer_objects:
+        cinema_bar.sell_product(customer.food, customer)
+
+    cinema_hall = CinemaHall(hall_number)
+    cinema_hall.movie_session(movie, customer_objects, cleaner)
