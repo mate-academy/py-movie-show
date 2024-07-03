d5f1527d925ef2ee97130ff7f03883a426affcd0
Oleksandr Vovk <165489096+VovkOL@users.noreply.github.com>
2024-07-03 08:35:29 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list, hall_number: int, cleaner: str, movie: str
+) -> None:
+
+    customer_objs = []
+    outputs = []
+    for customer in customers:
+        outputs.append(
+            f"Cinema bar sold {customer['food']} to {customer['name']}."
+        )
+        customer_obj = Customer(customer["name"], customer["food"])
+        customer_objs.append(customer_obj)
+
+    outputs.append(f'"{movie}" started in hall number {hall_number}.')
+    for customer_obj in customer_objs:
+        outputs.append(f'{customer_obj.name} is watching "{movie}".')
+    outputs.append(f'"{movie}" ended.')
+    outputs.append(f"Cleaner {cleaner} is cleaning hall number {hall_number}.")
+
+    for output in outputs:
+        print(output)
