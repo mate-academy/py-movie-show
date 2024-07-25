97fe021eb872ec2ee7563ca5116937665d66c229
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-25 21:35:29 +0300

    First try

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie_name: str
+) -> None:
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(number=hall_number)
+    cleaner_instance = Cleaner(name=cleaner)
+
+    customer_instances = [
+        Customer(name=cust["name"],
+                 food=cust["food"])
+        for cust in customers
+    ]
+    for cust in customer_instances:
+        cinema_bar.sell_product(product=cust.food, customer=cust)
+
+    cinema_hall.movie_session(movie_name=movie_name,
+                              customers=customer_instances,
+                              cleaning_staff=cleaner_instance
+                              )
---
+++ app/people/customer.py
@@ -0,0 +1,10 @@
+# from app.cinema.hall import CinemaHall
+
+
+class Customer:
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie_name: str) -> None:
+        print(f'{self.name} is watching "{movie_name}".')
