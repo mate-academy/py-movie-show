ae848d90be1a3fc57a51ec973f9ca81b6fc36d45
Ivan Shakhman <shakhmanivan@gmail.com>
2024-07-26 09:16:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(
-        customers: list, hall_number: int, cleaner: str, movie: str
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
 ) -> None:
-    cleaner_inst = Cleaner(cleaner)
     hall = CinemaHall(hall_number)
     clients = []
-    for customer in customers:
-        clients.append(Customer(customer["name"], customer["food"]))
-    for client in clients:
-        CinemaBar.sell_product(client.food, client)
-    hall.movie_session(movie, clients, cleaner_inst)
+    for i in range(len(customers)):
+        clients.append(Customer(customers[i]["name"], customers[i]["food"]))
+        CinemaBar.sell_product(clients[i].food, clients[i])
+    hall.movie_session(movie, clients, Cleaner(cleaner))
