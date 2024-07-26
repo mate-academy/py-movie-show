6cad269695de0a4f6847e144b69cb1b7cf20cfc1
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-07-26 13:34:11 +0300

    fix style

--- app/main.py
+++ app/main.py
@@ -5,21 +5,21 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-        customers: list, hall_number: int, cleaner: str, movie: str
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
 ) -> None:
-    # create Customers instances
     customers_obj = []
+
     for customer in customers:
         customers_obj.append(Customer(customer["name"], customer["food"]))
 
-    # create CinemaHall, CinemaBar, and Cleaner instances
     hall = CinemaHall(hall_number)
     cinema_bar = CinemaBar()
     cleaner_staff = Cleaner(cleaner)
 
-    # sell food to customers
     for customer in customers_obj:
         cinema_bar.sell_product(customer, customer.food)
 
-    # make a movie session
     hall.movie_session(movie, customers_obj, cleaner_staff)
