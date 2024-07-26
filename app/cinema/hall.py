2e231f70dcc0c80323176d7c5a7f879dba765c9f
Alexandr Politov <apolitov83@gmail.com>
2024-07-26 14:09:49 +0300

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -18,20 +18,3 @@ class CinemaHall:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
-
-
-# if __name__ == "__main__":
-#     customers = [
-#         {"name": "Bob", "food": "Coca-cola"},
-#         {"name": "Alex", "food": "popcorn"}
-#     ]
-#     number = 5
-#     cleaning_staff = "Anna"
-#     movie_name = "Madagascar"
-#     cs = CinemaHall(number)
-#     clean = Cleaner(cleaning_staff)
-#     some_customers = [
-#         Customer(customer["name"], customer["food"])
-#         for customer in customers
-#     ]
-#     cs.movie_session(movie_name, some_customers, clean)
