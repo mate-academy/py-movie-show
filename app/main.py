9803ea98ceae065f38e676ae6bde4965eb668b2d
Bogdan <bogdan.rylov422@gmail.com>
2024-07-29 11:25:42 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ def cinema_visit(
         movie: str
 ) -> None:
     cinema_hall = CinemaHall(hall_number)
-    customers_objects = [Customer(c["name"], c["food"]) for c in customers]
-    for customer in customers_objects:
-        CinemaBar.sell_product(customer.food, customer)
+    customers_objects = []
+    for customer in customers:
+        customer_obj = Customer(customer["name"], customer["food"])
+        customers_objects.append(customer_obj)
+        CinemaBar.sell_product(customer_obj.food, customer_obj)
     cinema_hall.movie_session(movie, customers_objects, Cleaner(cleaner))
