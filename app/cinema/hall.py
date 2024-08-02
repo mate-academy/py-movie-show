a0378957b779dea0ce6344370656c53a8865d102
Denys Piuro <denys.pyuro@gmail.com>
2024-08-02 15:31:01 +0300

    Fixed

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -4,13 +4,18 @@ from app.people.customer import Customer
 
 class CinemaHall:
 
-    def __init__(self, number: int) -> None:
+    def __init__(
+        self,
+        number: str
+    ) -> None:
         self.number = number
 
-    def movie_session(self,
-                      movie_name: str,
-                      customers: list[Customer],
-                      cleaning_staff: Cleaner) -> str:
+    def movie_session(
+        self,
+        movie_name: str,
+        customers: list[Customer],
+        cleaning_staff: Cleaner
+    ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
--- app/main.py
+++ app/main.py
@@ -4,18 +4,20 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customers_list = []
 
-    customers_list = [
-        Customer(name=customer_data["name"], food=customer_data["food"])
-        for customer_data in customers
-    ]
-
-    for customer in customers_list:
+    for customer_data in customers:
+        customer = Customer(
+            name=customer_data["name"], food=customer_data["food"]
+        )
         CinemaBar.sell_product(product=customer.food, customer=customer)
+        customers_list.append(customer)
 
     cinema_hall = CinemaHall(number=hall_number)
 
