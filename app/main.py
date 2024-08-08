ec27d249b83fba09d7548d7693c8c685c88e30c8
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-08 14:39:37 +0300

    fix code according to flake8

--- app/main.py
+++ app/main.py
@@ -4,9 +4,16 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    list_customers = [Customer(customer["name"], customer["food"]) for customer in customers]
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    list_customers = [Customer(customer["name"], customer["food"])
+                      for customer in customers]
     for customer in list_customers:
         CinemaBar.sell_product(customer.food, customer)
-    CinemaHall(hall_number).movie_session(movie, list_customers, Cleaner(cleaner))
-
+    CinemaHall(hall_number).movie_session(
+        movie,
+        list_customers,
+        Cleaner(cleaner)
+    )
