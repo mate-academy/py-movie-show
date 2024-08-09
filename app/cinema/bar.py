941d4c22ab9bb29ae5ac08581fc599199329349a
Ivan <vanya.bidnenko@gmail.com>
2024-08-09 15:05:01 +0300

    change annotations

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from app.people.customer import Customer
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: Customer, product: Customer) -> None:
+    def sell_product(customer: str, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
