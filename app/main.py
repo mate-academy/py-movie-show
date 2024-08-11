2babdb23d5b3788449b93eb9644ba749587cb63e
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-11 19:45:38 +0300

    Use list comprehension

--- app/main.py
+++ app/main.py
@@ -8,13 +8,11 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
-    customers_in_movie = []
+    customers_in_movie = [Customer(customer["name"], customer["food"])
+                          for customer in customers]
     hall = CinemaHall(hall_number)
     cleaner_stuff = Cleaner(cleaner)
     cinema_bar = CinemaBar()
-
-    for customer in customers:
-        customers_in_movie.append(Customer(customer["name"], customer["food"]))
 
     for customer_in_movie in customers_in_movie:
         cinema_bar.sell_product(customer_in_movie.food, customer_in_movie)
