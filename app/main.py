42b9deb5f325b669326508e93a203bdd96eba551
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-16 17:12:38 +0300

    Solution 3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-
 from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
 
@@ -11,21 +10,16 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-
-    cleaner_instance = Cleaner(name=cleaner)
-    customer_instances = [
-        Customer(name=customer["name"], food=customer["food"])
-        for customer in customers
-    ]
-
     cinema_hall = CinemaHall(number=hall_number)
     cinema_bar = CinemaBar()
 
-    for person in customer_instances:
-        cinema_bar.sell_product(
-            product=person.food,
-            customer=person
-        )
+    cleaner_instance = Cleaner(name=cleaner)
+    customer_instances = []
+
+    for customer in customers:
+        new_customer = Customer(name=customer["name"], food=customer["food"])
+        customer_instances.append(new_customer)
+        cinema_bar.sell_product(new_customer.food, new_customer)
 
     cinema_hall.movie_session(
         movie_name=movie,
