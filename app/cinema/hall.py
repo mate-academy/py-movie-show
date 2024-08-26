5f8cf9b46a7f7597b5a0a6761e33173558914934
Tetiana Sobko <sobkotiana@gmail.com>
2024-08-26 17:08:05 +0300

    Remove backslashes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,11 +9,11 @@ class CinemaHall:
     def movie_session(
             self, movie_name: str, customers: list[Customer], cleaner: Cleaner
     ) -> None:
-        print(f'\"{movie_name}\" started in hall number {self.number}.')
+        print(f'"{movie_name}" started in hall number {self.number}.')
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f'\"{movie_name}\" ended.')
+        print(f'"{movie_name}" ended.')
 
         cleaner.clean_hall(self.number)
