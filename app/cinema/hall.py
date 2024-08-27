ae36a807d669ee20c65d082baa96ef236909b9aa
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-27 22:00:30 +0300

    Maybe last solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -11,8 +11,10 @@ class CinemaHall:
         customers: list,
         cleaning_stuff: Cleaner
     ) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
+
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f"{movie_name} ended.")
+        print(f'"{movie_name}" ended.')
         cleaning_stuff.clean_hall(self.number)
