76702403ce020d425ae23a40e0206a75e864828f
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-05 21:28:59 +0300

    init cinema.hall module

---
+++ app/cinema/hall.py
@@ -0,0 +1,6 @@
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(movie_name: str, customers: list[Customer], cleaning_staff: Cleaner):
+        pass
