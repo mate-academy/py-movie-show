452557fa76fab0902556c59b9382ec7305cb37cb
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-05 23:08:12 +0300

    remove calling clean hall and rename iteration var

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    customers_list = [Customer(client["name"], client["food"]) for client in customers.items()]
+    customers_list = [Customer(customer["name"], customer["food"]) for customer in customers]
     cinema_hall = CinemaHall(hall_number)
     cleaner_staff = Cleaner(cleaner)
     cinema_bar = CinemaBar()
@@ -13,5 +13,5 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     for customer in customers_list:
         cinema_bar.sell_product(customer.food, customer)
 
-    cinema_hall.movie_session(movie, customers_list)
-    cleaner_staff.clean_hall(hall_number)
+    cinema_hall.movie_session(movie, customers_list, cleaner_staff)
+    
