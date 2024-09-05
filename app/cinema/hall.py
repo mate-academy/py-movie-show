e1dacd59760ee0b1cb64d601f7c70a59baf1e624
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-05 22:34:52 +0300

    complete movie_section method for cinema hall

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,6 +1,17 @@
+from app.people import customer
+from app.people import cinema_staff
+
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(movie_name: str, customers: list[Customer], cleaning_staff: Cleaner):
-        pass
+    def movie_session(self, movie_name: str,
+                      customers: list[customer.Customer], 
+                      cleaning_staff: cinema_staff.Cleaner):
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie_name)
+        
+        print(f"\"{movie_name}\" ended.")
+        cleaning_staff.clean_hall(self.number)
