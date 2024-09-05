d2a2b03074404cc284929bafa7dd0f250bd10f1a
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-05 23:56:56 +0300

    add doc strings to  cinema visit func in main file

--- app/main.py
+++ app/main.py
@@ -8,6 +8,29 @@ def cinema_visit(customers: list[dict[str, str]],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
+    """
+    Simulates a visit to the cinema, where customers buy food,
+    watch a movie, and the hall is cleaned.
+
+    Args:
+        customers (list[dict[str, str]]):
+            A list of dictionaries where each dictionary represents a customer,
+            containing 'name' (str) and 'food' (str) keys.
+        hall_number (int): The number of the cinema hall.
+        cleaner (str):
+            The name of the cleaner responsible for cleaning the hall.
+        movie (str): The name of the movie being shown.
+
+    Workflow:
+        1. Create a list of Customer instances based on the input data.
+        2. Create an instance of CinemaHall using the given hall number.
+        3. Create an instance of Cleaner using the cleaner's name.
+        4. Create an instance of CinemaBar to handle selling products.
+        5. Cinema bar sells food to each customer.
+        6. Start the movie session in the cinema hall,
+            where customers watch the movie.
+        7. After the movie ends, the cleaner cleans the cinema hall.
+    """
     customers_list: list[Customer] = [
         Customer(customer["name"], customer["food"]) for customer in customers
     ]
