d9ed7ca3da6feaf596bb96a1d3a8aabbbcd506f3
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2024-09-05 09:36:19 +0200

    Solution

---
+++ app/people/customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    customers = {}
+
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str) -> None:
+        print(f"""{self.name} is watching "{movie}".""")
