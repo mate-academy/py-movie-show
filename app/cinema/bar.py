279ec1de73b99995b056a98bca20ee2419eed290
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-05 22:38:26 +0300

    add import to cinema.bar

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,7 @@
+from app.people import customer
+
+
 class CinemaBar:
     @staticmethod
-    def sell_product(product: str, customer: Customer) -> None:
+    def sell_product(product: str, customer: customer.Customer) -> None:
         print(f"Cinema bar sold {product} to {customer}")
