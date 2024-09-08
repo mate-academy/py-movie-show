691dc55c4a9a6f4dbff916a2e0974a1405697510
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-09-08 21:53:09 +0300

    changed

--- app/main.py
+++ app/main.py
@@ -28,26 +28,3 @@ def cinema_visit(
         customers_instances,
         cleaner_instance
     )
-
-
-if __name__ == "__main__":
-    customers = [
-        {"name": "Bob", "food": "Coca-cola"},
-        {"name": "Alex", "food": "popcorn"}
-    ]
-    hall_number = 5
-    cleaner_name = "Anna"
-    movie = "Madagascar"
-    cinema_visit(
-        customers=customers,
-        hall_number=5,
-        cleaner="Anna",
-        movie="Madagascar"
-    )
-    # Cinema bar sold Coca-cola to Bob.
-    # Cinema bar sold popcorn to Alex.
-    # "Madagascar" started in hall number 5.
-    # Bob is watching "Madagascar".
-    # Alex is watching "Madagascar".
-    # "Madagascar" ended.
-    # Cleaner Anna is cleaning hall number 5.
