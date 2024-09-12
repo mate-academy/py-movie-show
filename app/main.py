26e07d459b9eb22b3203fa36e60fb701f6ce40ec
p1tonidze <p1t0nidze422@gmail.com>
2024-09-12 13:22:02 +0300

    fix flake8 py-cinema-visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from typing import List, Dict
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: List[Dict[str, str]],
+        hall_number: int,
+        cleaner: str, movie: str
+) -> None:
+    customer_objects = [
+        Customer(name=customer["name"],
+                 food=customer["food"]
+                 ) for customer in customers
+    ]
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(number=hall_number)
+    cleaner_object = Cleaner(name=cleaner)
+
+    for customer in customer_objects:
+        cinema_bar.sell_product(product=customer.food, customer=customer)
+
+    cinema_hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaner_object
+    )
