7cbd81cc7f798f2b62808f2326c459b427a92834
olgas <olgashydlovska@gmail.com>
2024-09-16 09:56:26 +0300

    Solution with appropriate annotations

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 class CinemaHall:
@@ -8,7 +9,7 @@ class CinemaHall:
 
     def movie_session(self,
                       movie_name: str,
-                      customers: list,
+                      customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
