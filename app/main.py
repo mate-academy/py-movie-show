0c5ed6d740b742783cf8424e262ee4325c40e5f0
Mike Shmygel <m.shmygel@gmail.com>
2024-09-18 11:09:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,32 +10,16 @@ def cinema_visit(
         cleaner_person: str,
         movie_title: str
 ) -> None:
-    cb = CinemaBar()
+    cinema_bar = CinemaBar()
     hall = CinemaHall(hall_num)
     cleaning_staff = Cleaner(cleaner_person)
 
     customer_objects = []
     for customer_data in customer_list:
         customer = Customer(customer_data["name"], customer_data["food"])
-        cb.sell_product(product=customer.food, customer=customer)
+        cinema_bar.sell_product(product=customer.food, customer=customer)
         customer_objects.append(customer)
 
     hall.movie_session(movie_name=movie_title,
                        customers=customer_objects,
                        cleaning_staff=cleaning_staff)
-
-
-customers = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-
-cinema_visit(
-    customer_list=customers,
-    hall_num=hall_number,
-    cleaner_person=cleaner_name,
-    movie_title=movie
-)
