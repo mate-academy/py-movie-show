3255a857e899a978e922bb3018714c9caf734bf2
Max <mxbely98@gmail.com>
2024-09-20 17:36:11 +0300

    Created "people" and "cinema" package then created next moduls: bar.py, cinema_staff.py, customer.py, hall.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str) -> None:
+    customer_list = [
+        Customer(
+            name=customer["name"],
+            food=customer["food"]
+        )
+        for customer in customers
+    ]
+    for customer in customer_list:
+        CinemaBar.sell_product(customer.food, customer)
+    hall = CinemaHall(hall_number)
+    cleaner_name = Cleaner(cleaner)
+    hall.movie_session(movie, customer_list, cleaner_name)
