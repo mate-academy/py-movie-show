5950edcf53ebd62910d46669e3add12990ca4f21
Andrii Ovod <ovodenok@mail.com>
2024-09-20 17:58:52 +0300

    Deleted useless comments

--- app/main.py
+++ app/main.py
@@ -7,16 +7,13 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: List[Dict[str, str]], hall_number: int,
                  cleaner: str, movie: str) -> None:
-    # Create instances of clients and cleaners
     customer_objects = [Customer(c["name"], c["food"]) for c in customers]
     cleaner_object = Cleaner(cleaner)
 
-    # Create an instance of bar and sell food
     cb = CinemaBar()
     for customer in customer_objects:
         cb.sell_product(customer.food, customer)
 
-    # Create instance of hall and start the session
     hall = CinemaHall(hall_number)
     hall.movie_session(movie_name=movie, customers=customer_objects,
                        cleaning_staff=cleaner_object)
