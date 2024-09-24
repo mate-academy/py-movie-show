9e3949b5aa5466509932c2b97aff1d2c6a173805
user <nikitaskrypets@gmail.com>
2024-09-24 12:44:33 +0300

    Solve style issues

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-import app.people.customer as customer
-import app.people.cinema_staff as cinema_staff
+import app.people.customer
+import app.people.cinema_staff
 
 
 class CinemaHall:
@@ -10,12 +10,11 @@ class CinemaHall:
     def movie_session(self,
                       movie_name: str,
                       customers: list,
-                      cleaning_staff: cinema_staff.Cleaner) -> None:
+                      cleaning_staff: app.people.cinema_staff.Cleaner) -> None:
 
         print(f'"{movie_name}" started in hall number {self.number}.')
-        for someone in customers:
-            one = customer.Customer(someone.name, "")
-            one.watch_movie(movie_name)
+        for customer in customers:
+            customer.watch_movie(movie_name)
 
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
