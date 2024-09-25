3d35e1455288fc9642ceeff3feb1707691ebcc21
user <nikitaskrypets@gmail.com>
2024-09-25 13:42:47 +0300

    Solved flake8 E126 and E501 problems

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-     customers: list,
-     hall_number: int,
-     cleaner: str,
-     movie: str
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
 ) -> None:
     all_customers = [Customer(someone["name"], someone["food"])
                      for someone in customers]
     for someone in all_customers:
         CinemaBar().sell_product(someone, someone.food)
-    CinemaHall(hall_number).movie_session(movie, all_customers, Cleaner(cleaner))
+    (CinemaHall(hall_number).
+     movie_session(movie, all_customers, Cleaner(cleaner)))
