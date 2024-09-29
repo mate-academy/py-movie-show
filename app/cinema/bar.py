5744c5d8fe9d42d7fa85adcfbbc245207147bf2a
Dima Havrylenko <94nj111@gmail.com>
2024-09-29 17:03:51 +0300

    implement Cinema

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -6,4 +6,4 @@ class CinemaBar:
 
     @staticmethod
     def sell_product(product: str, customer: Customer) -> None:
-        print(f'Cinema bar sold {product} to {customer.name}.')
+        print(f"Cinema bar sold {product} to {customer.name}.")
