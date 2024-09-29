cef3268fba97d32df70085f1c241d3500479288c
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-09-29 21:31:42 +0300

    import class «Cleaner» from module «cinema_staff.py» for module «main.py»

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
