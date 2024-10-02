0056b90aa6fd4c7d34a467ccd78ebcf6f05cff85
Bohdan Poliienko <mignatov756@gmail.com>
2024-10-02 14:16:45 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     all_visitors = [
         Customer(customer["name"],
                  customer["food"])
--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -4,4 +4,3 @@ class Cleaner:
 
     def clean_hall(self, hall_number: int) -> None:
         print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
-
