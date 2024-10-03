009f375ed14d0acebae3f543369157cd59720f4e
Alex <delete.rap.stupid@gmail.com>
2024-10-03 20:49:26 +0200

    solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,23 @@
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self,
+                      movie_name: str,
+                      customers: list,
+                      cleaning_staff: Cleaner) -> None:
+
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+
+        # move through the list of customers, which I take from main.py
+        for client in customers:
+
+            # print every client in cinema watching film
+            client.watch_movie(movie_name)
+
+        print(f"\"{movie_name}\" ended.")
+        cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+
+    # make the list of instances from making instances in file customer.py
+    list_of_viewers = [Customer(person.get("name"), person.get("food"))
+                       for person in customers]
+
+    # buying food from each instance in Customer
+    for viewer in list_of_viewers:
+        CinemaBar.sell_product(viewer.food, viewer)
+
+    # create instance of Cleaner in file cinema_staff.py
+    cleaner_today = Cleaner(cleaner)
+
+    # run the function which print start of movie, viewers and end of movie
+    CinemaHall(hall_number).movie_session(movie,
+                                          list_of_viewers,
+                                          cleaner_today)
