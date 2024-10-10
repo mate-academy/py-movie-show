3f3651ddfb91d1034db75aed5741a2a5b12ef1a6
Eldar <eldardemchenko19@mail.com>
2024-10-10 19:44:27 +0300

    fix_flake8

--- app/main.py
+++ app/main.py
@@ -4,10 +4,17 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from typing import List, Dict
 
 
-def cinema_visit(customers, hall_number, cleaner, movie):
-    customer_instances = [Customer(name=c['name'], food=c['food']) for c in customers]
+def cinema_visit(
+        customers: List[Dict[str, str]],
+        hall_number: int, cleaner: str, movie: str) -> None:
+
+    customer_instances = [
+        Customer(name=c["name"],
+                 food=c["food"]) for c in customers]
+
     cinema_bar = CinemaBar()
     cinema_hall = CinemaHall(number=hall_number)
     cleaner_instance = Cleaner(name=cleaner)
@@ -15,4 +22,7 @@ def cinema_visit(customers, hall_number, cleaner, movie):
     for customer in customer_instances:
         cinema_bar.sell_product(product=customer.food, customer=customer)
 
-    cinema_hall.movie_session(movie_name=movie, customers=customer_instances, cleaning_staff=cleaner_instance)
+    cinema_hall.movie_session(
+        movie_name=movie,
+        customers=customer_instances,
+        cleaning_staff=cleaner_instance)
--- app/people/customer.py
+++ app/people/customer.py
@@ -1,10 +1,9 @@
 # app/people/customer.py
 
 class Customer:
-    def __init__(self, name, food):
+    def __init__(self, name: str, food: str) -> None:
         self.name = name
         self.food = food
 
-    def watch_movie(self, movie):
+    def watch_movie(self, movie: any) -> None:
         print(f'{self.name} is watching "{movie}".')
-
