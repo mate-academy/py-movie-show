90f10c4b4872477130009c7c5a43b1a5a370bef0
Mykola <kutiamykola@gmail.com>
2024-10-29 08:48:36 +0200

    Solution_v2_MK

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,14 +7,14 @@ from typing import List
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
-        """Initialize with the hall number."""
+        """Initialize with the hall number. """
         self.number = number
 
     def movie_session(
         self, movie_name: str, customers: List[Customer],
             cleaning_staff: Cleaner
     ) -> None:
-        """Simulate a movie session with customers and cleaning afterward."""
+        """Simulate a movie session with customers and cleaning afterward. """
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
