dfc3d2efa45954905bb0e8a14b2ffa5d1286a5fa
unknown <bku089@gmail.com>
2024-10-31 00:11:07 +0200

    done3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from app.cinema import CinemaHall
-from app.people import Customer
-from app.people import Cleaner
-from app.cinema import CinemaBar
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 from typing import Any
 
 
@@ -20,3 +20,12 @@ def cinema_visit(
     for customer in customer_objects:
         CinemaBar.sell_product(customer.food, customer)
     hall.movie_session(movie, customer_objects, cleaning_staff)
+
+    customer_instances = []
+    for customer_data in customers:
+        customer = Customer(
+            name=customer_data["name"],
+            food=customer_data["food"]
+        )
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+        customer_instances.append(customer)
