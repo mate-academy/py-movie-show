ec127d62ea95b11ae6fe56d39f8a2dc7d4a8e433
unknown <bku089@gmail.com>
2024-10-30 22:00:55 +0200

    DONE

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ from app.cinema import CinemaBar
 from typing import Any
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> Any:
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> Any:
     hall = CinemaHall(hall_number)
     cleaning_staff = Cleaner(cleaner)
-    customer_objects = [Customer(customer["name"], customer["food"]) for customer in customers]
+    customer_objects = [
+        Customer(customer["name"], customer["food"])
+        for customer in customers
+    ]
     for customer in customer_objects:
         CinemaBar.sell_product(customer.food, customer)
     hall.movie_session(movie, customer_objects, cleaning_staff)
