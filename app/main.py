f744a7a177def1a3cd5ac73689f7dd2fd7799dc2
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-11-04 19:29:10 +0200

    7 push

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ def cinema_visit(
         customers: list,
         hall_number: int,
         cleaner: str,
-        movie: str) -> None:
+        movie: str
+) -> None:
     customer_instances = [Customer(customer["name"], customer["food"])
                           for customer in customers]
     for customer in customer_instances:
