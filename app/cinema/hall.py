b279e71ba45494c3703d4198040294af0f693077
Maskym Chukhno <chukhno.maksym@gmail.com>
2024-11-11 13:23:41 +0200

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,11 +7,11 @@ class CinemaHall:
                       customers: list,
                       cleaning_staff: str) -> str:
 
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'\"{movie_name}\" started in hall number {self.number}.')
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        print(f"\"{movie_name}\" ended.")
+        print(f'\"{movie_name}\" ended.')
 
         cleaning_staff.clean_hall(self.number)
--- app/people/customer.py
+++ app/people/customer.py
@@ -5,4 +5,4 @@ class Customer:
         self.food = food
 
     def watch_movie(self, movie: str) -> str:
-        print(f"{self.name} is watching \"{movie}\".")
+        print(f'{self.name} is watching \"{movie}\".')
