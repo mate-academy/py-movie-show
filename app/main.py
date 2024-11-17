c9747549ffce1fc4ad1fd10d15f4fcdbb230b671
shafran <shafran.andriy@gmail.com>
2024-11-17 18:14:11 +0200

    fixed3

--- app/main.py
+++ app/main.py
@@ -20,24 +20,3 @@ def cinema_visit(customers: [Customer],
     cinema_hall.movie_session(movie_name=movie,
                               customers=[Customer(**c) for c in customers],
                               cleaning_staff=cleaner)
-
-
-
-customers = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-hall_number = 5
-cleaner_name = "Anna"
-movie = "Madagascar"
-cinema_visit(customers=customers,
-             hall_number=hall_number,
-             cleaner=cleaner_name,
-             movie=movie)
-# Cinema bar sold Coca-cola to Bob.
-# Cinema bar sold popcorn to Alex.
-# "Madagascar" started in hall number 5.
-# Bob is watching "Madagascar".
-# Alex is watching "Madagascar".
-# "Madagascar" ended.
-# Cleaner Anna is cleaning hall number 5.
