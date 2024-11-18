e632dfb06aed8b4d7232d2883e8bbef88b69b3b8
Глеб Дубинин <gleb1395@gmail.com>
2024-11-18 22:07:20 +0200

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,9 +6,9 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customer: list[Customer], cleaning_staff: Cleaner): # NOQA E501
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+    def movie_session(self, movie_name: str, customer: list[Customer], cleaning_staff: Cleaner):  # NOQA E501
+        print(f"\"{movie_name}\" started in hall number {self.number}.")  # NOQA Q003
         for customer in customer:
             customer.watch_movie(movie_name)
-        print(f"\"{movie_name}\" ended.")
+        print(f"\"{movie_name}\" ended.")  # NOQA Q003
         cleaning_staff.clean_hall(self.number)
