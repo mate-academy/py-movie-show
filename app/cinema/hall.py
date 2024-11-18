44f6f7f3d661ecc2f9ecb536f51ebaf9177f49cd
Глеб Дубинин <gleb1395@gmail.com>
2024-11-18 22:41:37 +0200

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,7 +6,9 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customer: list[Customer], cleaning_staff: Cleaner):  # NOQA E501
+    def movie_session(self, movie_name: str,
+                      customer: list[Customer],
+                      cleaning_staff: Cleaner) -> None:
         print(f"\"{movie_name}\" started in hall number {self.number}.")  # NOQA Q003
         for customer in customer:
             customer.watch_movie(movie_name)
--- app/main.py
+++ app/main.py
@@ -6,14 +6,13 @@ from app.people.customer import Customer
 
 def cinema_visit(
     customers: list, hall_number: int, cleaner: str, movie: str
-) -> None:  # NOQA E501
+) -> None:
     customers_objects = [
         Customer(name=c["name"], food=c["food"]) for c in customers
-    ]  # NOQA E501
+    ]
     for customer in customers_objects:
         CinemaBar().sell_product(product=customer.food, customer=customer)
     clean = Cleaner(name=cleaner)
     CinemaHall(number=hall_number).movie_session(
-        movie_name=movie, customer=customers_objects, cleaning_staff=clean  # NOQA E501
-    )  # NOQA E501
-    pass
+        movie_name=movie, customer=customers_objects, cleaning_staff=clean
+    )
