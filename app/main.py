0b6b000e7577319d2b9e202c7d6a7f51ddd3337a
Глеб Дубинин <gleb1395@gmail.com>
2024-11-18 19:04:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 # write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:  # NOQA E501
+    customers_objects = [Customer(name=c["name"], food=c["food"]) for c in customers]  # NOQA E501
+    for customer in customers_objects:
+        CinemaBar().sell_product(product=customer.food, customer=customer)
+    clean = Cleaner(name=cleaner)
+    CinemaHall(number=hall_number).movie_session(movie_name=movie, customer=customers_objects,  # NOQA E501
+                                                 cleaning_staff=clean)  # NOQA E501
     pass
