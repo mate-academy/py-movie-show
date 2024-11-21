eea9447eab7cec505c88d6b2cd2af36e34505c02
Iryna Mishchenko <djrobentab@gmail.com>
2024-11-21 13:12:22 +0100

    changed quotation marks

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -5,4 +5,4 @@ class CinemaBar:
 
     @staticmethod
     def sell_product(product: str, customer: Customer) -> None:
-        print(f'Cinema bar sold {product} to {customer.name}.')
+        print(f"Cinema bar sold {product} to {customer.name}.")
