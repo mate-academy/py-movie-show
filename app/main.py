45599598c17fbb6999da03e9e2f721d6001e8d3f
Julia Alexeeva <julia4406@gmail.com>
2024-11-23 22:59:32 +0200

    Changed the order of parameters in cinema_visit, main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(movie: str,
-                 customers: list,
+def cinema_visit(customers: list,
                  hall_number: int,
-                 cleaner: str) -> None:
+                 cleaner: str,
+                 movie: str) -> None:
     instances_customers_list = []
     for customer in customers:
         customer = Customer(name=customer["name"], food=customer["food"])
