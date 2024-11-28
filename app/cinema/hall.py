ca160e5ed74d3f5967fdc5c1b6b4671c2719a534
Liza Shulika <liza.shulika2009@gmail.com>
2024-11-28 21:16:46 +0200

    cinema1

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,8 +9,8 @@ class CinemaHall:
     def movie_session(self, movie_name: str,
                       customer: list[Customer],
                       cleaning_staff: Cleaner) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}")
+        print(f'\"{movie_name}\" started in hall number {self.number}')
         for custome in customer:
             custome.watch_movie(movie=movie_name)
-        print(f"\"{movie_name}\" ended.")
+        print(f'\"{movie_name}\" ended.')
         cleaning_staff.clean_hall(hall_number=self.number)
