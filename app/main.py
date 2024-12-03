e5b27c86722ba865cb318663921ada2f5601d62d
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-03 18:15:31 +0200

    record of events in the cinema
    - added bar class
    - added hall class
    - added customer class
    - added cleaner class
    - added cinema_visit func wich invoke event loger

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-# write your imports here
+from __future__ import annotations
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    hall = CinemaHall(hall_number)
+    cleaner_instance = Cleaner(cleaner)
+    customers_instances = []
+
+    for customer in customers:
+        name, food = customer.values()
+        customer_instance = Customer(name, food)
+        CinemaBar.sell_product(food, customer_instance)
+        customers_instances.append(customer_instance)
+
+    hall.movie_session(movie, customers_instances, cleaner_instance)
