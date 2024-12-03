d97234c0d81aa14061c457dac86ce2949d9a8e9a
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-03 17:17:09 +0200

    Corrected the mentor's edits

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     peoples = [Customer(customer["name"], customer["food"])
                for customer in customers]
 
