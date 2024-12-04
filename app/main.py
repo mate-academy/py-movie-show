1a1bb348bc7f4cec9582e5e40c428e38903416f5
4ikkamony <compbanderloga@gmail.com>
2024-12-04 18:11:34 +0200

    Fixed cinema_visit argument order

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-        movie: str,
         customers: list[dict[str, str]],
         hall_number: int,
-        cleaner: str
+        cleaner: str,
+        movie: str
 ) -> None:
     cinema_hall = CinemaHall(hall_number)
 
