3193fbaf271a6cb2e025e49eb40e748f9da834d7
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-05 14:15:05 +0200

    Create task cinema visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+
+    list_of_customers = []
+
+    for customer in customers:
+        list_of_customers.append(Customer(customer["name"], customer["food"]))
+    cinema_hall = CinemaHall(hall_number)
+    cinema_bar = CinemaBar()
+    cleaner_staff = Cleaner(cleaner)
+
+    for customer in list_of_customers:
+        cinema_bar.sell_product(customer, customer.food)
+    cinema_hall.movie_session(movie, list_of_customers, cleaner_staff)
