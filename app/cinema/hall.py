f0d1a9942264fb823e7357d484e2c3be2618cb7e
Ruslan <ruslanyemets@gmail.com>
2024-12-05 18:34:52 +0200

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,5 +1,4 @@
 from app.people.customer import Customer
-
 from app.people.cinema_staff import Cleaner
 
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from app.cinema.bar import CinemaBar
-
 from app.cinema.hall import CinemaHall
-
 from app.people.customer import Customer
-
 from app.people.cinema_staff import Cleaner
 
 
@@ -23,8 +20,7 @@ def cinema_visit(
     for person in current_customers:
         CinemaBar.sell_product(person, person.food)
 
-    CinemaHall.movie_session(
-        cinema_hall,
+    cinema_hall.movie_session(
         movie,
         current_customers,
         current_cleaner
