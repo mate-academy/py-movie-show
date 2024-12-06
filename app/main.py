f35487d37a3d30116dbf4972bce7636fa468a115
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-06 19:05:50 +0200

    Cinema_Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
-def cinema_visit(
-                customers: list,
+
+def cinema_visit(customers: list,
                 hall_number: int,
                 cleaner: str,
                 movie: str) -> None:
