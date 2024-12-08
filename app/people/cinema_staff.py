b10457f048b5746852f916eaf53981a1c383bd2e
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-08 22:15:36 +0200

    Solution

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -5,7 +5,5 @@ class Cleaner:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def clean_hall(self) -> None:
-        print(
-            f"Cleaner {self.name} is cleaning hall number 
-            {CinemaHall.hall_number}")
+    def clean_hall(self, hall_number: int) -> None:
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}")
