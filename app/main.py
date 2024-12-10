df454b2298279ef65e0785136ee167cb4675200a
Taras <t.kudinov90@gmail.com>
2024-12-10 15:37:55 +0200

    Solved task with applied AI marks

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_num: int, clean: str, mov: str) -> None:
+    visitors = []
+    for client in customers:
+        customer = Customer(name=client["name"], food=client["food"])
+        visitors.append(customer)
+
+    staff = Cleaner(name=clean)
+    hall = CinemaHall(hall_num)
+
+    for customer in visitors:
+        CinemaBar.sell_product(customer, customer.food)
+
+    hall.movie_session(mov, visitors, staff)
