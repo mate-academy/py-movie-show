1f7de817bf7e0bd5e21252b406a88fe49f367aeb
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-10 20:51:54 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from app.cinema.bar import CinemaBar
-from app.cinema.hall import CinemaHall
-from app.people.cinema_staff import Cleaner
-from app.people.customer import Customer
+from cinema.bar import CinemaBar
+from cinema.hall import CinemaHall
+from people.cinema_staff import Cleaner
+from people.customer import Customer
 
 
 def cinema_visit(
