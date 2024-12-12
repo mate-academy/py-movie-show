fa211ca011940d288caf90a0f3fd1f6f96d43670
Olha Yukhnenko <pyolh36@gmail.com>
2024-12-12 14:21:55 +0200

    run_flake8

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,4 @@
 class CinemaBar:
     @staticmethod
-    def sell_product(product: str, customer) -> None:
+    def sell_product(product: str, customer: "Customer") -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -4,12 +4,27 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers, number, cleaner, movie):
-    customer_instances = [Customer(name=c["name"], food=c["food"]) for c in customers]
-    cleaner_instance = Cleaner(name=cleaner)
+def cinema_visit(
+        customers: list,
+        number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    customer_instances = [
+        Customer(
+            name=c["name"],
+            food=c["food"]
+        )
+        for c in customers
+    ]
+    cleaning_instance = Cleaner(name=cleaner)
     hall = CinemaHall(number=number)
 
     for customer in customer_instances:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
-    hall.movie_session(movie_name=movie, customers=customer_instances, cleaning_staff=cleaner_instance)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_instances,
+        cleaning_staff=cleaning_instance
+    )
