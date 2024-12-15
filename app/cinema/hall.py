d5c0b78b20c8f3394cfb37627f591f4dea5da907
nagafochfv <ohinskiy96@gmail.com>
2024-12-15 23:17:40 +0200

    'Solution'

---
+++ app/cinema/hall.py
@@ -0,0 +1,26 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: Customer,
+            cleaning_staff: Cleaner
+    ) -> None:
+        if not all(isinstance(customer, Customer) for customer in customers):
+            raise ValueError
+        if not isinstance(cleaning_staff, Cleaner):
+            raise ValueError
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie_name)
+
+        print(f"\"{movie_name}\" ended.")
+
+        cleaning_staff.clean_hall(self.number)
