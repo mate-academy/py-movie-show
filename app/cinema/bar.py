684cebce04207986c689c3068480c5cd3308c31d
Mykyta <mitina1900@gmail.com>
2025-01-07 21:48:28 +0200

    Solution

---
+++ app/cinema/bar.py
@@ -0,0 +1,3 @@
+class CinemaBar:
+    def sell_product(product: str, customer: Customer) -> None:
+        print(f" Cinema bar sold {product} to {customer}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-# write your imports here
-
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from people.cinema_staff import Cleaner
+from people.customer import Customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customer_objects = [Customer(name = c["name"], food =c["food"]) for c in customers]
+
+    for customer in customer_objects:
+        CinemaBar.sell_product(product = customer.food, customer = customer)
+
+    cinema_hall = CinemaHall(hall_number = hall_number)
+
+    cleaner_instance = Cleaner(name = cleaner)
+
+    cinema_hall.movie_session(movie_name = movie, customers = customer_objects, cleaning_staff = cleaner_instance)
