3ccb0a926b7d2b54c40abba431d075c6a8b975c4
Mykyta <mitina1900@gmail.com>
2025-01-07 22:19:52 +0200

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,3 +1,6 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
-    def sell_product(self, product: str, customer: "Customer") -> None:
+    def sell_product(self, product: str, customer: Customer) -> None:
         print(f" Cinema bar sold {product} to {customer}.")
