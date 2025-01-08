10969d9563911d0b6b8b24dc284319a3153a9630
Mykyta <mitina1900@gmail.com>
2025-01-08 13:07:18 +0200

    Solution

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -4,5 +4,5 @@ class Cleaner:
 
     def clean_hall(self, hall_number: int) -> None:
         print(
-            f"Cleaner {self.name} is cleaning hall hall_number {hall_number}."
+            f"Cleaner {self.name} is cleaning hall number {hall_number}."
         )
