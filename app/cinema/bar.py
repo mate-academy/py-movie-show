83480cb81a8c69f1b3669198154a93ceb6408e6f
Odin-max <meh84741@gmail.com>
2025-01-12 16:51:59 +0200

    123

---
+++ app/cinema/bar.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+class CinemaBar:
+    @staticmethod
+    def sell_product(product: str, customer: Any) -> str:
+        print(f"Cinema bar sold {product} to {customer.name}.")
