6fd4d4d6f4b20e719187169fe0c0397dde6ed824
Ngayka <ngayka@gmail.com>
2025-01-25 22:45:05 +0200

    Changed double qoutes everywhere

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,9 +5,9 @@ class CinemaHall:
     def movie_session(self, movie_name: str,
                       customers: str,
                       cleaning_staff: str) -> str:
-        print(f'"{movie_name}" started in hall number {self.number}.')
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f'"{movie_name}" ended.')
+        print(f"\"{movie_name}\" ended.")
 
         cleaning_staff.clean_hall(self.number)
