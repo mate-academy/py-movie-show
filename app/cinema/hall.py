8cd943db4410168e856483c3f15cbce8a3fa67a5
Zahar <galushkozahar>
2025-01-28 16:24:44 +0200

    Виконав

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -3,9 +3,11 @@
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
+
     def movie_session(self, movie_name: str, customers: list[Customer], cleaner: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
