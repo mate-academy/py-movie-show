14e93757eaad4bb3d7bb5c78d443267f01a7e7c7
Marek Smolana <msmolana@gmail.com>
2025-02-02 18:42:15 +0100

    added changes suggested by ai mentor

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,6 +12,7 @@ class CinemaHall:
             cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.hall_number}.')
-        (customer.watch_movie(movie_name) for customer in customers)
+        for customer in customers:
+            customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.hall_number)
