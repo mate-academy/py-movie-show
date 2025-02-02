b7ebf0299e079802944cd0087a84b6a3cd352ca1
Marek Smolana <msmolana@gmail.com>
2025-02-02 18:44:10 +0100

    fixes part 2

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from app.people.customer import Customer
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: Customer, food: str) -> None:
-        print(f"Cinema bar sold {food} to {customer.name}.")
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -5,16 +5,16 @@ from app.people.customer import Customer
 
 
 def cinema_visit(customers: list,
-                 number: int,
-                 cleaner: str,
+                 hall_number: int,
+                 cleaner_name: str,
                  movie: str) -> None:
-    cinema_hall = CinemaHall(number)
-    cleaning_staff = Cleaner(cleaner)
+    cinema_hall = CinemaHall(hall_number)
+    cleaning_staff = Cleaner(cleaner_name)
     customer_list = [Customer(customer["name"], customer["food"])
                      for customer in customers]
     customer_list.reverse()
     for customer in customer_list:
-        CinemaBar.sell_product(customer)
+        CinemaBar.sell_product(customer, customer.food)
 
     cinema_hall.movie_session(movie, customer_list, cleaning_staff)
     pass
